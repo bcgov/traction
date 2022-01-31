31c0ff1f723619409111fa16020ea6a73c87cb1a
Jason Sy <jasyrotuck@gmail.com>
2022-01-31 15:46:59 -0800

    transaction rollback works, but not accessible directly by test
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/traction/tests/conftest.py
+++ services/traction/tests/conftest.py
@@ -1,37 +1,31 @@
 import py
 import pytest
+import pytest_asyncio
 
 from api.main import app
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-from api.core.config import settings
+from api.db.session import engine, async_session
 
 
-engine = create_async_engine(
-    settings.SQLALCHEMY_DATABASE_URI,
-    echo=settings.DB_ECHO_LOG,
-    connect_args={"check_same_thread": False},
-)
-async_session = sessionmaker(
-    engine, class_=AsyncSession, autocommit=False, autoflush=False
-)
-
-
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture
 def client() -> TestClient:
     client = TestClient(app)
     return client
 
 
-@pytest.fixture(scope="session")
-def test_session() -> Session:
+@pytest_asyncio.fixture
+async def test_session(client) -> AsyncSession:
     try:
-        db = async_session()
-        yield db
+        conn = await engine.connect()
+        txn = await conn.begin()
+        sess = AsyncSession(bind=conn)
+        yield sess
     finally:
-        db.close()
+        await txn.rollback()
+        await conn.close()
--- services/traction/tests/endpoints/routes/test_innkeeper.py
+++ services/traction/tests/endpoints/routes/test_innkeeper.py
@@ -1,15 +1,26 @@
 import pytest
+import json
+import pprint
 from tests.db.tenant_factory import TenantFactory
 from api.db.repositories.tenants import TenantsRepository
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.testclient import TestClient
 
 
 @pytest.mark.asyncio
-async def test_tenants_get_all(client, test_session):
+async def test_tenants_get_all(client: TestClient, test_session: AsyncSession) -> None:
     _repo = TenantsRepository(test_session)
     test_tenant = TenantFactory.build()
-    out_tenant = await _repo.create(test_tenant)
-    print(test_tenant)
-    print(out_tenant)
+    test_session.add(test_tenant)
+    await test_session.commit()
+
     resp = client.get("/v1/innkeeper/tenants")
+
     assert resp.ok
-    assert False
+    resp_content = json.loads(resp.content)
+
+    pprint.PrettyPrinter().pprint(resp_content)
+    pprint.PrettyPrinter().pprint(client.__dict__)
+    pprint.PrettyPrinter().pprint(test_session.__dict__)
+
+    assert len(resp_content) == 1
