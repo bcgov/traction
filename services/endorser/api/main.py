1d0b4cde75587d7c42766ec34fff8c179d58b0c0
Jason Sherman <tools@usingtechnolo.gy>
2022-02-09 16:20:11 -0800

    test if the pr actions will ignore this...
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- services/endorser/api/main.py
+++ services/endorser/api/main.py
@@ -4,7 +4,6 @@ import time
 import uvicorn
 from fastapi import FastAPI
 
-# from api.db.errors import DoesNotExist, AlreadyExists
 from api.endpoints.routes.webhooks import get_webhookapp
 from api.core.config import settings
 from api.endorser_main import get_endorserapp
@@ -29,22 +28,6 @@ app.mount("/webhook", webhook_app)
 
 endorser_app = get_endorserapp()
 app.mount("/endorser", endorser_app)
-
-
-# @app.exception_handler(DoesNotExist)
-# async def does_not_exist_exception_handler(request: Request, exc: DoesNotExist):
-#     return JSONResponse(
-#         status_code=status.HTTP_404_NOT_FOUND,
-#         content={"message": str(exc)},
-#     )
-
-
-# @app.exception_handler(AlreadyExists)
-# async def already_exists_exception_handler(request: Request, exc: AlreadyExists):
-#     return JSONResponse(
-#         status_code=status.HTTP_409_CONFLICT,
-#         content={"message": str(exc)},
-#     )
 
 
 @app.get("/", tags=["liveness"])
