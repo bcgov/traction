15ad8956f645f8828aabb3413592e799bbb0695d
Lucas O'Neil <lucasoneil@gmail.com>
2022-02-10 16:33:44 -0800

    Vuetify and router

--- services/showcase/frontend/src/main.js
+++ services/showcase/frontend/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// import getRouter from '@/router';
+import vuetify from './plugins/vuetify';
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
--- services/showcase/frontend/vue.config.js
+++ services/showcase/frontend/vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? './'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+};
