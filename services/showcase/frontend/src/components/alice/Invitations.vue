27fac36b098e32958340dab65d95a3c1c970cb84
Lucas O'Neil <lucasoneil@gmail.com>
2022-02-17 21:59:00 -0800

    Scaffold
    Signed-off-by: Lucas O'Neil <lucasoneil@gmail.com>

---
+++ services/showcase/frontend/src/components/alice/Invitations.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Active Invitations</h2>
+    {{ currentSandbox }}
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Invitations',
+  data() {
+    return {
+      loading: false,
+    };
+  },
+  computed: {
+    ...mapGetters('sandbox', ['sandboxes', 'currentSandbox']),
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ services/showcase/frontend/src/views/Alice.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <h1 class="my-6 text-center">Alice's Profile</h1>
+    <Invitations />
+  </v-container>
+</template>
+
+<script>
+import Invitations from '@/components/alice/Invitations.vue';
+
+export default {
+  name: 'Alice',
+  components: {
+    Invitations
+  }
+};
+</script>
