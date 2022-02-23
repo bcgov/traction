f82cd6de40ea3257a4f19e917448cf0d250c08d5
Jason Sy <jasyrotuck@gmail.com>
2022-02-23 14:45:10 -0800

    remove patches.
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/showcase/api/main.py
+++ services/showcase/api/main.py
@@ -47,19 +47,6 @@ app = get_application()
 api = get_api()
 app.mount("/api", api)
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 class SPAStaticFiles(StaticFiles):
     async def get_response(self, path: str, scope):
