f2a84533087142bed4533cd96e5f0410611a8f5a
Lucas O'Neil <lucasoneil@gmail.com>
2022-02-22 16:52:32 -0800

    no sandbox text
    Signed-off-by: Lucas O'Neil <lucasoneil@gmail.com>

--- services/showcase/frontend/src/views/Alice.vue
+++ services/showcase/frontend/src/views/Alice.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-row class="mt-4">
+    <v-row class="mt-4" v-if="currentSandbox">
       <v-col md="3">
         <User />
       </v-col>
@@ -11,10 +11,15 @@
         <Messages />
       </v-col>
     </v-row>
+    <v-row v-else>
+      <p class="mt-10">No sandbox session set, please go to Innkeeper tab to set that up</p>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import Connections from '@/components/alice/Connections.vue';
 import Messages from '@/components/alice/Messages.vue';
 import User from '@/components/alice/User.vue';
@@ -25,6 +30,9 @@ export default {
     Connections,
     Messages,
     User,
+  },
+  computed: {
+    ...mapGetters('sandbox', ['currentSandbox']),
   },
 };
 </script>
