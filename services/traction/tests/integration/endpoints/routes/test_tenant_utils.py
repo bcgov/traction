e2971bff59127a166423ecb1542a5dd2127ddbec
Ian Costanzo <ian@anon-solutions.ca>
2022-03-10 15:21:13 -0800

    Tweak test timing
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- services/traction/tests/integration/endpoints/routes/test_tenant_utils.py
+++ services/traction/tests/integration/endpoints/routes/test_tenant_utils.py
@@ -21,7 +21,7 @@ from api.endpoints.models.credentials import CredPrecisForProof
 
 
 default_retry_attempts = int(
-    os.environ.get("DEFAULT_RETRY_ATTEMPTS", "10")
+    os.environ.get("DEFAULT_RETRY_ATTEMPTS", "11")
 )
 default_pause_between_attempts = int(
     os.environ.get("DEFAULT_PAUSE_BETWEEN_ATTEMPTS", "2")
