856576794be1ff79707d5cde29f0e6dfc32edbde
Jason Sy <jasyrotuck@gmail.com>
2022-03-18 14:26:36 -0700

    force formatting, UX review the content
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/showcase/frontend/src/components/alice/Connections.vue
+++ services/showcase/frontend/src/components/alice/Connections.vue
@@ -4,14 +4,36 @@
     <v-card-text v-if="credentials && credentials.length">
       <v-card v-for="c in credentials" :key="c.id" class="ma-3">
         <v-card-title class="grey lighten-3 mb-3">{{
-          c.cred_def_id
+          'Education Degree' || c.cred_def_id
         }}</v-card-title>
         <v-card-text>
-          <template v-for="(value, key) in c.attrs">
-            <div :key="key">
-              <b>{{ key }}:</b> {{ value }}
-            </div>
-          </template>
+          <ul>
+            <li><b>Degree: </b>{{ c.attrs.degree }}</li>
+            <li><b>Completion Date: </b>{{ c.attrs.date }}</li>
+            <li><b>Name: </b>{{ c.attrs.name }}</li>
+            <li><b>Age: </b>{{ c.attrs.age }}</li>
+            <li><b>Student ID: </b>{{ c.attrs.student_id }}</li>
+            <hr />
+            <li>
+              <b>Issuer Name: </b>
+              <a target="_blank" :href="issuer_nym_link(c.cred_def_id)">
+                {{ issuer_name() }}
+              </a>
+            </li>
+            <li>
+              <b> Connection to Issuer: </b>
+              Active
+            </li>
+          </ul>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header> Details </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <div><b>cred_def_id:</b> {{ c.cred_def_id }}</div>
+                <div><b>schema_id:</b> {{ c.schema_id }}</div>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
         </v-card-text>
       </v-card>
     </v-card-text>
@@ -36,6 +58,14 @@ export default {
   },
   methods: {
     ...mapActions('alice', ['getCredentials']),
+    issuer_name() {
+      return 'Faber University';
+    },
+    issuer_nym_link(cred_def_id) {
+      return `http://test.bcovrin.vonx.io/browse/domain?page=1&query=${
+        cred_def_id.split(':')[0]
+      }&txn_type=1`;
+    },
   },
   async mounted() {
     await this.getCredentials();
