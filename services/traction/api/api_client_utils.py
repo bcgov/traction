32b15145f1786b2b8439bf63e37db921fc790270
Lucas ONeil <lucasoneil@gmail.com>
2022-04-21 14:28:52 -0700

    try url
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/traction/api/api_client_utils.py
+++ services/traction/api/api_client_utils.py
@@ -5,6 +5,6 @@ from api.core.config import settings
 
 
 def get_api_client():
-    configuration = Configuration(host=settings.ACAPY_ADMIN_URL)
+    configuration = Configuration(host="http://traction-agent:8031")
     api_client = ApiClient(configuration=configuration)
     return api_client
