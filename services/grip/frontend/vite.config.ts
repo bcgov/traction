ed5f1792c70bb4b82dd731f53094101f9630137b
Jamie Popkin <popkinj@littleearth.ca>
2022-06-07 15:43:19 -0700

    Here is Vite
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/grip/frontend/vite.config.ts
+++ services/grip/frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import styleImport, { VantResolve } from "vite-plugin-style-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    styleImport({
+      // https://vitejs.dev/plugin-style-import
+      resolves: [VantResolve()],
+    }),
+  ],
+});
