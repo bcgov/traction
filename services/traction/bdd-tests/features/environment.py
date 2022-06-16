70cfb0aadb2c94ff13183b1ef35ee0c95a20752d
Jason Sherman <tools@usingtechnolo.gy>
2022-06-16 15:01:40 -0700

    add BDD test for auto respond
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- services/traction/bdd-tests/features/environment.py
+++ services/traction/bdd-tests/features/environment.py
@@ -1,6 +1,20 @@
 from pprint import PrettyPrinter, pprint
+
+from behave import register_type
+
 from steps.setup import _hard_delete_tenant
 from pprint import pp
+
+
+def parse_boolean(text):
+    if text.lower() == "true":
+        return True
+    elif text.lower() == "false":
+        return False
+    raise ValueError("Expect True or False, got {}".format(text))
+
+
+register_type(bool=parse_boolean)
 
 
 def after_scenario(context, scenario):
