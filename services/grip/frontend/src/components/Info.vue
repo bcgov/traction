7322ce9c71f29c8fc0b3c1b4c39f2f35987afb0e
Jamie Popkin <popkinj@littleearth.ca>
2022-06-28 14:28:30 -0700

    Wallet info events working well
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/grip/frontend/src/components/Info.vue
+++ services/grip/frontend/src/components/Info.vue
@@ -1,9 +1,52 @@
 <script setup lang="ts">
 import { inject } from "vue";
+import Fieldset from "primevue/fieldset";
+import ProgressSpinner from "primevue/progressspinner";
+import axios from "axios";
 
 const store: any = inject("store");
+
+const toggled = (e: any) => {
+  console.log("toggled");
+  store.state.walletInfo.open = !e.value;
+  if (store.state.walletInfo.open && !store.state.walletInfo.data) {
+    console.log("Fetching wallet info");
+    axios
+      .get("http://localhost:5100/tenant/v1/admin/self", {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${store.state.token}`,
+        },
+      })
+      .then((res) => {
+        console.log("res", res);
+        store.state.walletInfo.data = res.data.item;
+      })
+      .catch((err) => {
+        store.state.walletInfo.data = null;
+        console.error("error", err);
+      });
+  }
+};
 </script>
 
-<template>Here is our token {{ store.state.token }}</template>
+<template>
+  <Fieldset
+    :toggleable="true"
+    :collapsed="!store.state.walletInfo.open"
+    @toggle="toggled"
+  >
+    <template #legend> Wallet Info </template>
+    <!-- Token: {{ store.state.token }} -->
+    <ProgressSpinner v-if="!store.state.walletInfo.data" />
+    <div v-else>hey there</div>
+  </Fieldset>
+</template>
 
-<style></style>
+<style scoped>
+fieldset {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
