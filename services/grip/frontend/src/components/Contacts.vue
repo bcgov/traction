5b4b7b1600c8987b44a89d4240c477fff4e593f1
Jamie Popkin <popkinj@littleearth.ca>
2022-06-30 15:54:53 -0700

    Make requests to the /contacts end point
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/grip/frontend/src/components/Contacts.vue
+++ services/grip/frontend/src/components/Contacts.vue
@@ -9,8 +9,27 @@ const store: any = inject("store");
 // Vite passes the api url to here
 const api = import.meta.env.VITE_TRACTION_ENDPOINT;
 
-const toggled = () => {
-  console.log("toggled");
+const toggled = (e: any) => {
+  store.state.contacts.open = !e.value;
+  console.log("toggled", store.state.contacts.open);
+
+  if (store.state.contacts.open && !store.state.contacts.data) {
+    axios
+      .get(`${api}/tenant/v1/contacts`, {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${store.state.token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        store.state.contacts.data = res.data.items;
+      })
+      .catch((err) => {
+        store.state.contacts.data = null;
+        console.error("error", err);
+      });
+  }
 };
 </script>
 
@@ -21,6 +40,8 @@ const toggled = () => {
     @toggle="toggled"
   >
     <template #legend>Contacts</template>
+
+    <ProgressSpinner />
   </Fieldset>
 </template>
 
