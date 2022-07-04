f685e6ac3496118c24bd7ed8597e059f7261b194
Jason Syrotuck <jasyrotuck@gmail.com>
2022-07-04 15:58:24 -0700

    Feature/qr code invitation (#209)
    accidentally merged the wrong way, some changes are reporting oddly. 
    
    add very simple create contact form. takes alias and gives back url, and FE renders it in a QR code and provides a 'copy to clipboard' button. 
    
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

---
+++ services/grip/frontend/src/components/TenantDashboard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { inject } from "vue";
+import Info from "./Info.vue";
+import CreateContact from "./CreateContact.vue";
+
+const store: any = inject("store");
+</script>
+
+<template>
+  <div>
+    <Info />
+    <CreateContact />
+  </div>
+</template>
+
+
+<script lang="ts">
+export default {
+  name: "TenantDashboard",
+};
+</script>
+
+<style></style>
---
+++ services/grip/frontend/src/components/common/QRCode.vue
@@ -0,0 +1,61 @@
+
+<script setup lang="ts">
+import { ref } from "vue";
+import QrcodeVue from "qrcode.vue";
+import Accordion from "primevue/accordion";
+import AccordionTab from "primevue/accordiontab";
+import { useToast } from "vue-toastification";
+
+const toast = useToast();
+
+const copy_to_clipboard = (content: string) => {
+  navigator.clipboard.writeText(content);
+  toast("QR Code contents copied to clipboard");
+  return;
+};
+</script>
+
+
+
+<template>
+  <div class="qr-container" v-if="qr_content">
+    <qrcode-vue
+      class="qr-image"
+      v-if="qr_content"
+      :value="qr_content"
+      :size="400"
+      level="H"
+    />
+    <Accordion>
+      <AccordionTab header="View Raw Content">
+        <i @click="copy_to_clipboard(qr_content)" class="pi pi-paperclip"></i>
+        <!-- {{ qr_content }} -->
+      </AccordionTab>
+    </Accordion>
+  </div>
+  <span v-else>No Content Found</span>
+</template>
+
+
+
+<script lang="ts">
+export default {
+  name: "QRCode",
+  props: {
+    qr_content: String,
+  },
+};
+</script>
+
+<style>
+.qr-image {
+  display: flex;
+  margin-bottom: 25px;
+}
+.qr-container {
+  max-width: 400px;
+}
+i {
+  margin: 10px;
+}
+</style>
