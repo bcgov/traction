1e4930bd81efc2c96123a51ad44f37a22dca416d
Jason Sy <jasyrotuck@gmail.com>
2022-07-19 12:36:49 -0700

    fix
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/traction/bdd-tests/features/steps/security.py
+++ services/traction/bdd-tests/features/steps/security.py
@@ -17,14 +17,10 @@ def step_impl(context, tenant_1: str, tenant_2: str, tenant_2_contact_alias: str
     )
     assert response1.status_code == status.HTTP_200_OK, response1.__dict__
     tenant_2_contact = json.loads(response1.content)["items"][0]
-    print(tenant_2_contact)
-
-    tenant_2_contact_id = tenant_2_contact["contact_id"]
-    print(tenant_2_contact_id)
 
     response2 = requests.get(
         context.config.userdata.get("traction_host")
-        + f"/tenant/v1/contacts/{tenant_2_contact_id}",
+        + f"/tenant/v1/contacts/{tenant_2_contact['contact_id']}",
         headers=context.config.userdata[tenant_1]["auth_headers"],
     )
-    assert response2.status_code == status.HTTP_404_OK, response2.__dict__
+    assert response2.status_code == status.HTTP_404_NOT_FOUND, response2.__dict__
