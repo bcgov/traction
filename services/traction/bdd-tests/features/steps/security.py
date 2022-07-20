9b729ee32b376a94ca2d27bc42910bd01c0a5b9f
Jason Sy <jasyrotuck@gmail.com>
2022-07-20 16:15:49 -0700

    use http helpers
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/traction/bdd-tests/features/steps/security.py
+++ services/traction/bdd-tests/features/steps/security.py
@@ -3,6 +3,7 @@ import json
 
 from behave import *
 from starlette import status
+from v1_api import *
 
 
 @then(
@@ -10,17 +11,9 @@ from starlette import status
 )
 def step_impl(context, tenant_1: str, tenant_2: str, tenant_2_contact_alias: str):
 
-    response1 = requests.get(
-        context.config.userdata.get("traction_host") + "/tenant/v1/contacts",
-        params={"alias": tenant_2_contact_alias},
-        headers=context.config.userdata[tenant_2]["auth_headers"],
-    )
+    response1 = list_contacts(context, tenant_2, {"alias": tenant_2_contact_alias})
     assert response1.status_code == status.HTTP_200_OK, response1.__dict__
     tenant_2_contact = json.loads(response1.content)["items"][0]
 
-    response2 = requests.get(
-        context.config.userdata.get("traction_host")
-        + f"/tenant/v1/contacts/{tenant_2_contact['contact_id']}",
-        headers=context.config.userdata[tenant_1]["auth_headers"],
-    )
+    response2 = get_contact(context, tenant_1, tenant_2_contact["contact_id"])
     assert response2.status_code == status.HTTP_404_NOT_FOUND, response2.__dict__
