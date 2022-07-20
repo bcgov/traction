81359f07d05a0c8a8f5da750615a664fac1b9988
Jason Sy <jasyrotuck@gmail.com>
2022-07-20 16:25:06 -0700

    move it back
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/traction/api/endpoints/dependencies/tenant_security.py
+++ services/traction/api/endpoints/dependencies/tenant_security.py
@@ -135,3 +135,13 @@ async def get_tenant_access_token(
             "t_id": tenant["tenant_id"],
         }
     )
+
+
+def get_from_context(name: str):
+    result = context.get(name)
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Error not authenticated",
+        )
+    return result
