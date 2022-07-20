22d645e709e66ca6eb2f6379fc1cb7a7b97848d0
Jamie Popkin <popkinj@littleearth.ca>
2022-07-20 15:46:03 -0700

    Schema attributes
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/grip/frontend/src/components/CreateSchema.vue
+++ services/grip/frontend/src/components/CreateSchema.vue
@@ -5,6 +5,7 @@ import Dropdown from "primevue/dropdown";
 import { ref } from "vue";
 const alias = ref("");
 const alias1 = ref("");
+const alias2 = ref("");
 
 // Schema types
 const options = [
@@ -13,10 +14,18 @@ const options = [
   { name: "Boolean", code: "Boolean" },
 ];
 
+const addAttribute = () => {
+  console.log(alias.value);
+  console.log(alias1.value);
+  console.log(alias2.value);
+  console.log(selectedOption.value);
+};
+
+const selectedOption = ref(null);
+
 // Store the selected option
 // TODO: This needs to be turned into an array that
 //   grows and shrinks with the number of attributes.
-const selectedOption = ref(null);
 </script>
 <template>
   <div class="row">
@@ -25,23 +34,35 @@ const selectedOption = ref(null);
       <label for="create_schema_name">Name</label>
     </span>
     <span class="p-float-label">
-      <InputText type="text" v-model="alias" name="create_schema_version" />
+      <InputText type="text" v-model="alias1" name="create_schema_version" />
       <label for="create_schema_version">Version Number</label>
     </span>
   </div>
   <hr />
   <div class="row">
     <span class="p-float-label">
-      <InputText type="text" v-model="alias1" name="create_schema_name" />
+      <InputText type="text" v-model="alias2" name="create_schema_name" />
       <label for="create_schema_name">Attribute</label>
     </span>
     <Dropdown
       v-model="selectedOption"
       :options="options"
       optionLabel="name"
-      placeholder="Format"
+      placeholder="Type"
     />
   </div>
+  <div style="float: right">
+    <button
+      class="p-button p-component p-button-icon-only p-button-rounded p-button-outlined"
+      type="button"
+      @click="addAttribute"
+    >
+      <span class="pi pi-plus p-button-icon"></span>
+    </button>
+  </div>
+  <br />
+  <br />
+  <hr />
   <Button label="Save"></Button>
 </template>
 <style scoped>
@@ -49,5 +70,6 @@ const selectedOption = ref(null);
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  margin: 20px 0;
 }
 </style>
