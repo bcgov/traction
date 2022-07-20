419da2c81f855788325d2f706dd91ecc54e696cd
Jamie Popkin <popkinj@littleearth.ca>
2022-07-20 09:55:05 -0700

    Here is the dropdown
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/grip/frontend/src/components/CreateSchema.vue
+++ services/grip/frontend/src/components/CreateSchema.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
+import Dropdown from "primevue/dropdown";
 import { ref } from "vue";
 const alias = ref("");
 const alias1 = ref("");
+
+// Schema types
+const options = [
+  { name: "Text", code: "String" },
+  { name: "Number", code: "Number" },
+  { name: "Boolean", code: "Boolean" },
+];
+
+// Store the selected option
+const selectedOption = ref(null);
+
+const onChange = (e: any) => {
+  console.log(e.value);
+  console.log(selectedOption);
+};
 </script>
 <template>
   <div class="row">
@@ -22,6 +38,13 @@ const alias1 = ref("");
       <InputText type="text" v-model="alias1" name="create_schema_name" />
       <label for="create_schema_name">Attribute</label>
     </span>
+    <Dropdown
+      v-model="selectedOption"
+      :options="options"
+      optionLabel="name"
+      placeholder="Format"
+      @change="onChange"
+    />
   </div>
   <Button label="Save"></Button>
 </template>
