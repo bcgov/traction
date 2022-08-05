48b6cadd84ce5d0b05f96a1f7801252674a974c8
Jason Sherman <tools@usingtechnolo.gy>
2022-08-05 12:18:38 -0700

    remove integtest (no longer used)
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- services/traction/tests/unit/test_main.py
+++ services/traction/tests/unit/test_main.py
@@ -3,11 +3,5 @@ import pytest
 pytestmark = pytest.mark.asyncio
 
 
-@pytest.mark.integtest
-async def test_hello_world(test_client):
-    resp = await test_client.get("/")
-    assert resp.status_code == 200
-
-
 async def test_world():
     assert True
