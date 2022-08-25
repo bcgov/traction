bf0b0633983e974ae205989fb677a16e40b01c5c
Jason Sherman <tools@usingtechnolo.gy>
2022-08-25 15:05:42 -0700

    TRACTION-371 - form to create cred def (template) will work for either created or copied schema. limiting to a single cred template for now. hack in place to list cred templates with schema templates, add new ticket to put in API. form looks bad... styling and making layout universal is another ticket, this is purely functional.
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

---
+++ services/tenant-ui/frontend/src/components/issuance/credentialtemplate/CreateCredentialTemplate.vue
@@ -0,0 +1,32 @@
+<template>
+    <Button
+        icon="pi pi-id-card" 
+        class="p-button-text"        
+        @click="displayForm = true"
+        v-tooltip.top="'Create Credential Template'"
+    />
+    <Dialog header="Create Credential Template" v-model:visible="displayForm">
+        <CreateCredentialTemplateForm 
+            @form-close="displayForm = false;"
+            :schema-template-id="schemaTemplateId"
+        />
+    </Dialog>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Dialog from "primevue/dialog";
+import Button from "primevue/button";
+
+import CreateCredentialTemplateForm from "./CreateCredentialTemplateForm.vue";
+
+
+const props = defineProps({
+    schemaTemplateId: {
+        type: String,
+        required: true
+    }
+});
+
+const displayForm = ref(false);
+
+</script>
