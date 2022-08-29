2e6fbb525e1c10b9120134e00b51d9e573893ae9
Jamie Popkin <popkinj@littleearth.ca>
2022-08-29 13:27:40 -0700

    Refresh schemas button
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/tenant-ui/frontend/src/components/issuance/Schemas.vue
+++ services/tenant-ui/frontend/src/components/issuance/Schemas.vue
@@ -3,20 +3,40 @@
 
   <ProgressSpinner v-if="loading" />
   <div v-else>
-    <DataTable v-model:selection="selectedSchemaTemplate" :value="schemaTemplates" :paginator="true" :rows="10"
-      striped-rows selection-mode="single">
+    <DataTable
+      v-model:selection="selectedSchemaTemplate"
+      :value="schemaTemplates"
+      :paginator="true"
+      :rows="10"
+      striped-rows
+      selection-mode="single"
+    >
       <template #header>
         <div class="flex justify-content-between">
           <span class="p-input-icon-left">
             <i class="pi pi-search" />
-            <InputText v-model="schemaTemplateFilters" placeholder="Schema Search" disabled />
+            <InputText
+              v-model="schemaTemplateFilters"
+              placeholder="Schema Search"
+              disabled
+            />
           </span>
         </div>
+        <Button
+          icon="pi pi-refresh"
+          class="p-button-rounded p-button-outlined btn-refresh-table"
+          title="Refresh Table"
+          @click="loadTable"
+        ></Button>
       </template>
       <Column :sortable="false" header="Actions">
         <template #body="{ data }">
-          <Button title="Delete Schema" icon="pi pi-times"
-            class="p-button-rounded p-button-icon-only p-button-danger p-button-text" @click="deleteSchema($event, data)" />
+          <Button
+            title="Delete Schema"
+            icon="pi pi-times"
+            class="p-button-rounded p-button-icon-only p-button-danger p-button-text"
+            @click="deleteSchema($event, data)"
+          />
         </template>
       </Column>
       <Column field="name" header="Schema" filter-field="name" />
@@ -27,23 +47,46 @@
       <Column field="schema_id" header="ID" />
       <Column field="credential_templates" header="Credential Template">
         <template #body="{ data }">
-          <CreateCredentialTemplate :schema-template-id="data.schema_template_id"
-            v-if="!data.credential_templates.length" />
+          <CreateCredentialTemplate
+            :schema-template-id="data.schema_template_id"
+            v-if="!data.credential_templates.length"
+          />
           <div v-else>
-            {{ `${data.credential_templates[0].name}:${data.credential_templates[0].tag}` }}
+            {{
+              `${data.credential_templates[0].name}:${data.credential_templates[0].tag}`
+            }}
           </div>
         </template>
       </Column>
     </DataTable>
     <div class="row buttons">
-      <Button v-if="schemaTemplates" class="create-btn" icon="pi pi-plus" label="Create Schema"
-        @click="createSchema"></Button>
-      <Button v-if="schemaTemplates" class="copy-btn" icon="pi pi-copy" label="Copy Schema" @click="copySchema" />
+      <Button
+        v-if="schemaTemplates"
+        class="create-btn"
+        icon="pi pi-plus"
+        label="Create Schema"
+        @click="createSchema"
+      ></Button>
+      <Button
+        v-if="schemaTemplates"
+        class="copy-btn"
+        icon="pi pi-copy"
+        label="Copy Schema"
+        @click="copySchema"
+      />
     </div>
-    <Dialog v-model:visible="displayCreateSchema" header="Create a new schema" :modal="true">
+    <Dialog
+      v-model:visible="displayCreateSchema"
+      header="Create a new schema"
+      :modal="true"
+    >
       <CreateSchema @success="schemaCreated" />
     </Dialog>
-    <Dialog v-model:visible="displayCopySchema" header="Copy an existing schema" :modal="true">
+    <Dialog
+      v-model:visible="displayCopySchema"
+      header="Copy an existing schema"
+      :modal="true"
+    >
       <CopySchema @success="schemaCopied" />
     </Dialog>
   </div>
@@ -106,12 +149,12 @@ const schemaCreated = async () => {
 const deleteSchema = (event: any, schema: any) => {
   confirm.require({
     target: event.currentTarget,
-    message: 'Are you sure you want to delete this schema?',
-    header: 'Confirmation',
-    icon: 'pi pi-exclamation-triangle',
+    message: "Are you sure you want to delete this schema?",
+    header: "Confirmation",
+    icon: "pi pi-exclamation-triangle",
     accept: () => {
       doDelete(schema);
-    }
+    },
   });
 };
 const doDelete = (schema: any) => {
@@ -153,5 +196,10 @@ const schemaCopied = async () => {
 
 .create-btn {
   margin-right: 1rem;
+}
+.btn-refresh-table {
+  position: absolute;
+  right: 12px;
+  top: 12px;
 }
 </style>
