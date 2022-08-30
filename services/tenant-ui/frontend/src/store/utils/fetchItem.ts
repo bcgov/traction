13d0c66fe6297c5eadcf58d98e148ddabc7a0275
Jason Sy <jasyrotuck@gmail.com>
2022-08-30 10:45:50 -0700

    fetch item comment
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/tenant-ui/frontend/src/store/utils/fetchItem.ts
+++ services/tenant-ui/frontend/src/store/utils/fetchItem.ts
@@ -2,6 +2,14 @@ import { useTenantApi } from '../tenantApi';
 import { Ref } from 'vue';
 
 export async function fetchItem(url: string, id: string, dict: any, error: Ref<any>, loading: Ref<boolean>, params: any = {}) {
+    /*
+    This method will fetch an item from the api and add it to a dictionary in the store, 
+    to be retrieved by the primary key value later.
+
+    TODO: fix dict typing, should be a Ref<object> but my typescript linter doesn't like that.
+    */
+
+
     const tenantApi = useTenantApi();
     console.log(`> fetchItem(${url})`);
     error.value = null;
@@ -10,7 +18,7 @@ export async function fetchItem(url: string, id: string, dict: any, error: Ref<a
         .getHttp(url + id, params)
         .then((res) => {
             dict.value[id] = res.data.item;
-            console.log(dict[id]);
+            console.log(dict.value[id]);
         })
         .catch((err) => {
             error.value = err;
