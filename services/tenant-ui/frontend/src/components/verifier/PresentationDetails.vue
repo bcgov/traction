5180ec4b2baabb388bfb5e6069ac94f961106ec0
Jason Sy <jasyrotuck@gmail.com>
2022-08-30 12:33:05 -0700

    if presentation exchange not complete
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/tenant-ui/frontend/src/components/verifier/PresentationDetails.vue
+++ services/tenant-ui/frontend/src/components/verifier/PresentationDetails.vue
@@ -5,7 +5,7 @@
           <li>Updated at: {{props.presentation.updated_at}}</li>
           <li>Contact Alias: {{props.presentation.contact.alias}}</li>
           <hr/>
-            <li v-for="value, attr in props.presentation.acapy.presentation_exchange.presentation.requested_proof.revealed_attr_groups">
+            <li v-for="value, attr in props.presentation.acapy.presentation_exchange.presentation?.requested_proof.revealed_attr_groups">
               <h4>{{attr}}:</h4>
               <span v-for="val, attr_name in value.values" class="presentation-attr-value">
                 <b>{{attr_name}}</b> : {{val.raw}}
