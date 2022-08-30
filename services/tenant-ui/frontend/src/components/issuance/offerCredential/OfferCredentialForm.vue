97bf41ccb6d499c028915b84c518e5c65817b7a7
Lucas ONeil <lucasoneil@gmail.com>
2022-08-29 17:28:45 -0700

    Cred editing (JSON)
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/components/issuance/offerCredential/OfferCredentialForm.vue
+++ services/tenant-ui/frontend/src/components/issuance/offerCredential/OfferCredentialForm.vue
@@ -1,55 +1,79 @@
 <template>
     <div>
         <form @submit.prevent="handleSubmit(!v$.$invalid)">
+            <!-- Main Form -->
+            <div v-if="!showEditCredValues">
+                <!-- Credential -->
+                <div class="field">
+                    <label for="selectedCred" :class="{ 'p-error': v$.selectedCred.$invalid && submitted }">Credential
+                        Name
+                        <ProgressSpinner v-if="ctLoad" />
+                    </label>
 
-
-            <!-- Credential -->
-            <div class="field">
-                <label for="selectedCred" :class="{ 'p-error': v$.selectedCred.$invalid && submitted }">Credential
-                    Name
-                    <ProgressSpinner v-if="ctLoad" />
-                </label>
-
-                <AutoComplete id="selectedCred" v-model="v$.selectedCred.$model" :disabled="ctLoad"
-                    :suggestions="filteredCreds" @complete="searchCreds($event)" :dropdown="true" optionLabel="label"
-                    forceSelection />
-                <small v-if="v$.selectedCred.$invalid && submitted" class="p-error">{{
-                        v$.selectedCred.required.$message
-                }}</small>
-            </div>
-
-            <!-- Contact -->
-            <div class="field mt-5">
-                <label for="selectedContact" :class="{ 'p-error': v$.selectedContact.$invalid && submitted }">Connection
-                    Name
-                    <ProgressSpinner v-if="cLoad" />
-                </label>
-
-                <AutoComplete id="selectedContact" v-model="v$.selectedContact.$model" :disabled="cLoad"
-                    :suggestions="filteredContacts" @complete="searchContacts($event)" :dropdown="true"
-                    optionLabel="label" forceSelection />
-                <small v-if="v$.selectedContact.$invalid && submitted" class="p-error">{{
-                        v$.selectedContact.required.$message
-                }}</small>
-            </div>
-
-            <!-- Cred Values -->
-            <div class="field mt-5">
-                <div class="flex justify-content-between">
-                    <label for="credentialValues" class="flex justify-content-start"
-                        :class="{ 'p-error': v$.credentialValues.$invalid && submitted }">Credential Value</label>
-                    <Button label="Enter Credential Value" class="p-button-link flex justify-content-end"
-                        :disabled="!v$.selectedCred.$model" />
+                    <AutoComplete id="selectedCred" v-model="v$.selectedCred.$model" :disabled="ctLoad"
+                        :suggestions="filteredCreds" @complete="searchCreds($event)" :dropdown="true"
+                        optionLabel="label" forceSelection />
+                    <small v-if="v$.selectedCred.$invalid && submitted" class="p-error">{{
+                            v$.selectedCred.required.$message
+                    }}</small>
                 </div>
-                <Textarea id="credentialValues" v-model="v$.credentialValues.$model"
-                    :class="{ 'w-full': true, 'p-invalid': v$.credentialValues.$invalid && submitted }"
-                    :autoResize="true" rows="20" readonly />
-                <small v-if="v$.credentialValues.$invalid && submitted" class="p-error">{{
-                        v$.credentialValues.required.$message
-                }}</small>
+
+                <!-- Contact -->
+                <div class="field mt-5">
+                    <label for="selectedContact"
+                        :class="{ 'p-error': v$.selectedContact.$invalid && submitted }">Connection
+                        Name
+                        <ProgressSpinner v-if="cLoad" />
+                    </label>
+
+                    <AutoComplete id="selectedContact" v-model="v$.selectedContact.$model" :disabled="cLoad"
+                        :suggestions="filteredContacts" @complete="searchContacts($event)" :dropdown="true"
+                        optionLabel="label" forceSelection />
+                    <small v-if="v$.selectedContact.$invalid && submitted" class="p-error">{{
+                            v$.selectedContact.required.$message
+                    }}</small>
+                </div>
+
+                <!-- Cred Values -->
+                <div class="field mt-5">
+                    <div class="flex justify-content-between">
+                        <label for="credentialValues" class="flex justify-content-start"
+                            :class="{ 'p-error': v$.credentialValues.$invalid && submitted }">Credential Value</label>
+                        <Button label="Enter Credential Value" class="p-button-link flex justify-content-end"
+                            :disabled="!v$.selectedCred.$model" @click="editCredentialValues" />
+                    </div>
+                    <Textarea id="credentialValues" v-model="v$.credentialValues.$model"
+                        :class="{ 'w-full': true, 'p-invalid': v$.credentialValues.$invalid && submitted }"
+                        :autoResize="true" rows="20" cols="50" readonly />
+                    <small v-if="v$.credentialValues.$invalid && submitted" class="p-error">{{
+                            v$.credentialValues.required.$message
+                    }}</small>
+                </div>
+
+                <Button type="submit" label="Send Offer" class="mt-5 w-full" :disabled="loading" :loading="loading" />
             </div>
 
-            <Button type="submit" label="Send Offer" class="mt-5 w-full" :disabled="loading" :loading="loading" />
+            <!-- Credential values -->
+            <div v-else>
+                <Button icon="pi pi-arrow-left" class="p-button-rounded p-button-text mr-2 pt-3"
+                    @click="showEditCredValues = false" />
+                <strong>{{ v$.selectedCred.$model.label }}</strong>
+
+                <div class="field mt-5">
+                    <label for="credentialValues"
+                        :class="{ 'p-error': v$.credentialValues.$invalid && submitted }">Credential Field
+                        Values
+                        <small>(Schema: {{ schemaForSelectedCred.schema_name }} {{ schemaForSelectedCred.version
+                        }})</small>
+                    </label>
+                    <!-- TODO: This is replaced with the dynamic field creation  -->
+                    <Textarea id="credentialValuesEdit" v-model="v$.credentialValuesEditing.$model" :autoResize="true"
+                        rows="20" cols="50" class="w-full" />
+
+                    <Button label="Save" class="mt-5 w-full" @click="saveCredValues" />
+                </div>
+
+            </div>
         </form>
     </div>
 </template>
@@ -60,6 +84,7 @@ import { computed, reactive, ref } from "vue";
 // PrimeVue / Validation
 import AutoComplete from 'primevue/autocomplete';
 import Button from "primevue/button";
+import Textarea from "primevue/textarea";
 import ProgressSpinner from "primevue/progressspinner";
 import { required } from "@vuelidate/validators";
 import { useVuelidate } from "@vuelidate/core";
@@ -76,20 +101,24 @@ const {
     loading: cLoad, contactsDropdown
 } = storeToRefs(useContactsStore());
 const {
-    loading: ctLoad, credentialTemplateDropdown
+    loading: ctLoad, credentialTemplateDropdown, credentialTemplates, schemaTemplates
 } = storeToRefs(useGovernanceStore());
 const loading = computed(() => cLoad.value || ctLoad.value)
 
 // Form and Validation
+// TODO: this one replaced with dynamic field display
 const filteredCreds = ref();
 const filteredContacts = ref();
+const schemaForSelectedCred = ref({});
 const showEditCredValues = ref(false);
 const formFields = reactive({
+    credentialValuesEditing: '',
     credentialValues: '',
     selectedContact: '',
     selectedCred: ''
 });
 const rules = {
+    credentialValuesEditing: {},
     credentialValues: { required },
     selectedCred: { required },
     selectedContact: { required }
@@ -119,6 +148,34 @@ const searchCreds = (event: any) => {
     };
 };
 
+// Editing the credential
+const editCredentialValues = () => {
+    showEditCredValues.value = true;
+
+    // Popuplate cred editor if it's not already been edited
+    console.log(formFields.credentialValues.length);
+    if (!formFields.credentialValues.length) {
+        // Get the specific schema to edit values for
+        const schemaId = credentialTemplates.value.find(ct => ct.credential_template_id === formFields.selectedCred.value).schema_template_id;
+        const schema = schemaTemplates.value.find(st => st.schema_template_id === schemaId);
+        schemaForSelectedCred.value = schema;
+
+        const schemaFillIn = schemaForSelectedCred.value.attributes.map(a => {
+            return {
+                name: `${a}`,
+                value: ""
+            }
+        });
+        console.log(schemaFillIn);
+        formFields.credentialValuesEditing = JSON.stringify(schemaFillIn, 0, 2);
+    }
+}
+const saveCredValues = () => {
+    // TODO: to be replaced with dynamic form field component
+    formFields.credentialValues = formFields.credentialValuesEditing;
+    showEditCredValues.value = false;
+}
+
 // Form submission
 const submitted = ref(false);
 const handleSubmit = async (isFormValid: boolean) => {
@@ -136,6 +193,7 @@ const handleSubmit = async (isFormValid: boolean) => {
 .field>small.p-error {
     display: block !important;
 }
+
 .field>label>.p-progress-spinner {
     height: 1em;
     width: 1em;
