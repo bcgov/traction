0f3bb95563b521b2c8a17ed1581d704267cd72e8
Jason Sherman <tools@usingtechnolo.gy>
2022-09-06 15:59:40 -0700

    standardize copy schema
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- services/tenant-ui/frontend/src/components/issuance/Schemas.vue
+++ services/tenant-ui/frontend/src/components/issuance/Schemas.vue
@@ -1,93 +1,78 @@
 <template>
   <h3 class="mt-0">Schemas</h3>
 
-  <ProgressSpinner v-if="loading" />
-  <div v-else>
-    <DataTable
-      v-model:selection="selectedSchemaTemplate"
-      :value="schemaTemplates"
-      :paginator="true"
-      :rows="10"
-      striped-rows
-      selection-mode="single"
-    >
-      <template #header>
-        <div class="flex justify-content-between">
-          <span class="p-input-icon-left">
-            <i class="pi pi-search" />
-            <InputText
-              v-model="schemaTemplateFilters"
-              placeholder="Schema Search"
-              disabled
-            />
-          </span>
+  <DataTable
+    v-model:selection="selectedSchemaTemplate"
+    :loading="loading"
+    :value="schemaTemplates"
+    :paginator="true"
+    :rows="10"
+    striped-rows
+    selection-mode="single"
+  >
+    <template #header>
+      <div class="flex justify-content-between">
+        <div class="flex justify-content-start">
+          <CopySchema class="ml-4" />
+        </div>
+        <div class="flex justify-content-end">
           <Button
             icon="pi pi-refresh"
             class="p-button-rounded p-button-outlined"
             title="Refresh Table"
             @click="loadTable"
-          ></Button>
+          />
+        </div>
+      </div>
+    </template>
+    <template #empty> No schemas found. </template>
+    <template #loading> Loading schema data. Please wait... </template>
+    <Column :sortable="false" header="Actions">
+      <template #body="{ data }">
+        <Button
+          title="Delete Schema"
+          icon="pi pi-times"
+          class="p-button-rounded p-button-icon-only p-button-danger p-button-text"
+          @click="deleteSchema($event, data)"
+        />
+      </template>
+    </Column>
+    <Column field="name" header="Schema" filter-field="name" />
+    <Column field="version" header="Version" />
+    <Column field="status" header="Status" />
+    <Column field="state" header="State" />
+    <Column field="attributes" header="Attributes" />
+    <Column field="schema_id" header="ID" />
+    <Column field="credential_templates" header="Credential Template">
+      <template #body="{ data }">
+        <CreateCredentialTemplate
+          v-if="!data.credential_templates.length"
+          :schema-template-id="data.schema_template_id"
+        />
+        <div v-else>
+          {{
+            `${data.credential_templates[0].name}:${data.credential_templates[0].tag}`
+          }}
         </div>
       </template>
-      <Column :sortable="false" header="Actions">
-        <template #body="{ data }">
-          <Button
-            title="Delete Schema"
-            icon="pi pi-times"
-            class="p-button-rounded p-button-icon-only p-button-danger p-button-text"
-            @click="deleteSchema($event, data)"
-          />
-        </template>
-      </Column>
-      <Column field="name" header="Schema" filter-field="name" />
-      <Column field="version" header="Version" />
-      <Column field="status" header="Status" />
-      <Column field="state" header="State" />
-      <Column field="attributes" header="Attributes" />
-      <Column field="schema_id" header="ID" />
-      <Column field="credential_templates" header="Credential Template">
-        <template #body="{ data }">
-          <CreateCredentialTemplate
-            v-if="!data.credential_templates.length"
-            :schema-template-id="data.schema_template_id"
-          />
-          <div v-else>
-            {{
-              `${data.credential_templates[0].name}:${data.credential_templates[0].tag}`
-            }}
-          </div>
-        </template>
-      </Column>
-    </DataTable>
-    <div class="row buttons">
-      <Button
-        v-if="schemaTemplates"
-        class="create-btn"
-        icon="pi pi-plus"
-        label="Create Schema"
-        @click="createSchema"
-      ></Button>
-      <Button
-        v-if="schemaTemplates"
-        class="copy-btn"
-        icon="pi pi-copy"
-        label="Copy Schema"
-        @click="copySchema"
-      />
-    </div>
+    </Column>
+  </DataTable>
+  <div class="row buttons">
+    <Button
+      v-if="schemaTemplates"
+      class="create-btn"
+      icon="pi pi-plus"
+      label="Create Schema"
+      @click="createSchema"
+    ></Button>
+  </div>
+  <div>
     <Dialog
       v-model:visible="displayCreateSchema"
       header="Create a new schema"
       :modal="true"
     >
       <CreateSchema @success="schemaCreated" />
-    </Dialog>
-    <Dialog
-      v-model:visible="displayCopySchema"
-      header="Copy an existing schema"
-      :modal="true"
-    >
-      <CopySchema @success="schemaCopied" />
     </Dialog>
   </div>
 </template>
@@ -97,13 +82,11 @@ import { ref, onMounted } from 'vue';
 import Button from 'primevue/button';
 import Column from 'primevue/column';
 import DataTable from 'primevue/datatable';
-import InputText from 'primevue/inputtext';
-import ProgressSpinner from 'primevue/progressspinner';
 import Dialog from 'primevue/dialog';
 
 // Custom components
 import CreateSchema from './CreateSchema.vue';
-import CopySchema from './CopySchema.vue';
+import CopySchema from './copySchema/CopySchema.vue';
 import CreateCredentialTemplate from './credentialtemplate/CreateCredentialTemplate.vue';
 
 import { useConfirm } from 'primevue/useconfirm';
@@ -167,18 +150,6 @@ const doDelete = (schema: any) => {
       console.error(err);
       toast.error(`Failure: ${err}`);
     });
-};
-
-const displayCopySchema = ref(false);
-const copySchema = () => {
-  displayCopySchema.value = !displayCopySchema.value;
-};
-const schemaCopied = async () => {
-  // this is not getting called... bug? or need to find a new pattern (works on Contacts).
-  console.log(
-    'schema copied emit - do we want to "manually" load contacts or have the store automatically do it?'
-  );
-  loadTable();
 };
 
 // -----------------------------------------------/Loading schemas
