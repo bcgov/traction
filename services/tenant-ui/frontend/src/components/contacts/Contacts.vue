869dfc2ea6f639acf24fd2d52344baefb91d4fab
Jason Sherman <tools@usingtechnolo.gy>
2022-09-06 15:30:13 -0700

    remove progress spinner, use datatable loading (allows binding of table data plus control over modal)
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- services/tenant-ui/frontend/src/components/contacts/Contacts.vue
+++ services/tenant-ui/frontend/src/components/contacts/Contacts.vue
@@ -1,48 +1,48 @@
 <template>
   <h3 class="mt-0">Connections</h3>
 
-  <ProgressSpinner v-if="loading" />
-  <div v-else>
-    <DataTable
-      :value="contacts"
-      :paginator="true"
-      :rows="10"
-      striped-rows
-      selection-mode="single"
-    >
-      <template #header>
-        <div class="flex justify-content-between">
-          <CreateContact />
-          <Button
-            icon="pi pi-refresh"
-            class="p-button-rounded p-button-outlined"
-            title="Refresh Table"
-            @click="loadTable"
-          ></Button>
-        </div>
+  <DataTable
+    v-model:selection="selectedContact"
+    :loading="loading"
+    :value="contacts"
+    :paginator="true"
+    :rows="10"
+    striped-rows
+    selection-mode="single"
+  >
+    <template #header>
+      <div class="flex justify-content-between">
+        <CreateContact />
+        <Button
+          icon="pi pi-refresh"
+          class="p-button-rounded p-button-outlined"
+          title="Refresh Table"
+          @click="loadTable"
+        ></Button>
+      </div>
+    </template>
+    <template #empty> No contacts found. </template>
+    <template #loading> Loading contacts data. Please wait... </template>
+    <Column :sortable="true" field="alias" header="Name" />
+    <Column field="role" header="Role" />
+    <Column field="state" header="State" />
+    <Column field="status" header="Status" />
+    <Column field="created_at" header="Created at">
+      <template #body="{ data }">
+        {{ formatDateLong(data.created_at) }}
       </template>
-      <Column :sortable="true" field="alias" header="Name" />
-      <Column field="role" header="Role" />
-      <Column field="state" header="State" />
-      <Column field="status" header="Status" />
-      <Column field="created_at" header="Created at">
-        <template #body="{ data }">
-          {{ formatDateLong(data.created_at) }}
-        </template>
-      </Column>
-      <Column field="contact_id" header="ID" />
-    </DataTable>
-  </div>
+    </Column>
+    <Column field="contact_id" header="ID" />
+  </DataTable>
 </template>
 
 <script setup lang="ts">
 // Vue
-import { ref, onMounted } from 'vue';
+import { onMounted } from 'vue';
 // PrimeVue
 import Button from 'primevue/button';
 import Column from 'primevue/column';
 import DataTable from 'primevue/datatable';
-import ProgressSpinner from 'primevue/progressspinner';
 
 // State
 import { useContactsStore } from '@/store';
@@ -57,20 +57,13 @@ const toast = useToast();
 
 const contactsStore = useContactsStore();
 
-// do not use the loading from the store as that will cause a refresh of components...
-// .. including the create invitation dialog (it needs to stay open)
-const loading = ref(false);
-const { contacts } = storeToRefs(useContactsStore());
+const { loading, contacts, selectedContact } = storeToRefs(useContactsStore());
 
 const loadTable = async () => {
-  loading.value = true;
-  contactsStore
-    .listContacts()
-    .then(() => (loading.value = false))
-    .catch((err) => {
-      console.error(err);
-      toast.error(`Failure: ${err}`);
-    });
+  contactsStore.listContacts().catch((err) => {
+    console.error(err);
+    toast.error(`Failure: ${err}`);
+  });
 };
 
 onMounted(async () => {
