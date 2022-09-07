82c794744edb5259324e09238a4df59f9021f0c4
Jason Sy <jasyrotuck@gmail.com>
2022-09-07 13:42:31 -0700

    move after merge conflict
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- services/tenant-ui/frontend/src/components/issuance/createSchema/CreateSchema.vue
+++ services/tenant-ui/frontend/src/components/issuance/createSchema/CreateSchema.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <Button label="Create Schema" icon="pi pi-plus" @click="openModal" />
+    <Button
+      label="Create Schema"
+      :disabled="!isIssuer"
+      icon="pi pi-plus"
+      @click="openModal"
+    />
     <Dialog
       v-model:visible="displayModal"
       header="Create Schema"
@@ -19,11 +24,14 @@ import { ref } from 'vue';
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
 // State
+import { useTenantStore } from '../../store';
 
 // Custom Components
 import CreateSchemaForm from './CreateSchemaForm.vue';
 // Other Imports
 import { useToast } from 'vue-toastification';
+const tenantStore = useTenantStore();
+const { isIssuer } = storeToRefs(useTenantStore());
 
 // State setup
 
