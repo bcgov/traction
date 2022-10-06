7746bd9cb560f5608078c5b64ff215d447a24a1e
Lucas ONeil <lucasoneil@gmail.com>
2022-10-06 16:52:48 -0700

    Tenant Table
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

---
+++ services/tenant-ui/frontend/src/components/innkeeper/tenants/CheckInTenant.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Hi
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ services/tenant-ui/frontend/src/store/innkeeper/innkeeperTenantsStore.ts
@@ -0,0 +1,69 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { useInnkeeperApi } from './innkeeperApi';
+import { fetchList } from '../utils';
+
+export const useInnkeeperTenantsStore = defineStore('innkeeperTenants', () => {
+  // state
+  const tenants: any = ref(null);
+  const loading: any = ref(false);
+  const error: any = ref(null);
+
+  // getters
+
+  // actions
+
+  // grab the tenant api
+  const innkeeperApi = useInnkeeperApi();
+
+  async function listTenants() {
+    return fetchList('/innkeeper/v1/tenants/', tenants, error, loading);
+  }
+
+  async function checkInTenant(name: string, allowIssue: boolean) {
+    console.log('> innkeeperTenantsStore.checkInTenant');
+    error.value = null;
+    loading.value = true;
+
+    let tenant_data = null;
+    await innkeeperApi
+      .postHttp('/innkeeper/v1/tenants/check-in', {
+        name: name,
+        allow_issue_credentials: allowIssue,
+      })
+      .then((res) => {
+        tenant_data = res.data;
+      })
+      .then(() => {
+        // Tenant created, reload the list in state
+        listTenants();
+      })
+      .catch((err) => {
+        error.value = err;
+        console.error(error.value);
+      })
+      .finally(() => {
+        loading.value = false;
+      });
+    console.log('< innkeeperTenantsStore.checkInTenant');
+
+    if (error.value != null) {
+      // throw error so $onAction.onError listeners can add their own handler
+      throw error.value;
+    }
+    // return data so $onAction.after listeners can add their own handler
+    return tenant_data;
+  }
+
+  return {
+    loading,
+    error,
+    tenants,
+    listTenants,
+    checkInTenant,
+  };
+});
+
+export default {
+  useInnkeeperTenantsStore,
+};
