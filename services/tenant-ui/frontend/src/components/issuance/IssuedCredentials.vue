f65024a462861be0d00257b9e35063d6fcbc3657
Lucas ONeil <lucasoneil@gmail.com>
2022-10-20 22:55:42 -0700

    Delete and revoke cred
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/components/issuance/IssuedCredentials.vue
+++ services/tenant-ui/frontend/src/components/issuance/IssuedCredentials.vue
@@ -25,6 +25,27 @@
     <template #empty> No records found. </template>
     <template #loading> Loading data. Please wait... </template>
     <Column :expander="true" header-style="width: 3rem" />
+    <Column header="Actions">
+      <template #body="{ data }">
+        <Button
+          title="Delete Credential"
+          icon="pi pi-trash"
+          class="p-button-rounded p-button-icon-only p-button-text mr-2"
+          @click="deleteCredential($event, data)"
+        />
+        <Button
+          v-if="
+            data.credential_template &&
+            data.credential_template.revocation_enabled &&
+            !data.revoked
+          "
+          title="Revoke Credential"
+          icon="pi pi-times-circle"
+          class="p-button-rounded p-button-icon-only p-button-text"
+          @click="revokeCredential($event, data)"
+        />
+      </template>
+    </Column>
     <Column
       :sortable="true"
       field="credential_template.name"
@@ -41,7 +62,6 @@
         {{ formatDateLong(data.created_at) }}
       </template>
     </Column>
-    <Column field="revoked" header="Revoked?" />
     <template #expansion="{ data }">
       <RowExpandData
         :id="data.issuer_credential_id"
@@ -63,6 +83,7 @@ import Button from 'primevue/button';
 import Column from 'primevue/column';
 import DataTable from 'primevue/datatable';
 import { useToast } from 'vue-toastification';
+import { useConfirm } from 'primevue/useconfirm';
 // Other Components
 import OfferCredential from './offerCredential/OfferCredential.vue';
 import RowExpandData from '../common/RowExpandData.vue';
@@ -70,6 +91,7 @@ import { formatDateLong } from '@/helpers';
 import StatusChip from '../common/StatusChip.vue';
 
 const toast = useToast();
+const confirm = useConfirm();
 
 const issuerStore = useIssuerStore();
 // use the loading state from the store to disable the button...
@@ -77,13 +99,57 @@ const { loading, credentials, selectedCredential } = storeToRefs(
   useIssuerStore()
 );
 
+// Delete a specific cred
+const deleteCredential = (event: any, data: any) => {
+  confirm.require({
+    target: event.currentTarget,
+    message: 'Are you sure you want to DELETE this credential?',
+    header: 'Confirmation',
+    icon: 'pi pi-exclamation-triangle',
+    accept: () => {
+      issuerStore
+        .deleteCredential(data.issuer_credential_id)
+        .then(() => {
+          toast.success(`Credential deleted`);
+        })
+        .catch((err) => {
+          console.error(err);
+          toast.error(`Failure: ${err}`);
+        });
+    },
+  });
+};
+
+// Revoke a specific cred
+const revokeCredential = (event: any, data: any) => {
+  confirm.require({
+    target: event.currentTarget,
+    message: 'Are you sure you want to REVOKE this credential?',
+    header: 'Confirmation',
+    icon: 'pi pi-exclamation-triangle',
+    accept: () => {
+      issuerStore
+        .revokeCredential({
+          issuer_credential_id: data.issuer_credential_id,
+        })
+        .then(() => {
+          toast.success(`Credential revoked`);
+        })
+        .catch((err) => {
+          console.error(err);
+          toast.error(`Failure: ${err}`);
+        });
+    },
+  });
+};
+
+// Get the credentials
 const loadTable = async () => {
   await issuerStore.listCredentials().catch((err) => {
     console.error(err);
     toast.error(`Failure: ${err}`);
   });
 };
-
 onMounted(async () => {
   await loadTable();
 });
