2e0193d2d5ddee018f69f27b88f73a8377759a07
Jamie Popkin <popkinj@littleearth.ca>
2022-11-01 14:36:28 -0700

    A safety catch for the for/in loop
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/tenant-ui/frontend/src/i18n/i18n.ts
+++ services/tenant-ui/frontend/src/i18n/i18n.ts
@@ -6,8 +6,11 @@ function loadLocaleMessages() {
   const modules = import.meta.glob('./locales/*.json');
   const messages: any = {};
   for (const path in modules) {
+    console.log('here is a path', path);
     // For each file found, build the message object
     // with the file NAME as the top level key
+    if (modules.hasOwnProperty(path)) continue;
+
     modules[path]().then((mod: any) => {
       const matches = path.match(/[ \w-]+?(?=\.)/i);
       if (matches && matches.length) {
