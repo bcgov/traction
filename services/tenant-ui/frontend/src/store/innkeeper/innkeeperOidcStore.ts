f78f1f4573718ea6013613cfc9e74bb31b0f004b
Lucas ONeil <lucasoneil@gmail.com>
2022-11-07 21:28:35 -0800

    Config
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/store/innkeeper/innkeeperOidcStore.ts
+++ services/tenant-ui/frontend/src/store/innkeeper/innkeeperOidcStore.ts
@@ -1,31 +1,27 @@
 import axios from 'axios';
-import { defineStore } from 'pinia';
+import { defineStore, storeToRefs } from 'pinia';
 import { computed, ref } from 'vue';
 import { useConfigStore } from '../configStore';
-import {
-  UserManager,
-  WebStorageStateStore,
-  User,
-  UserManagerEvents,
-} from 'oidc-client-ts';
+import { useInnkeeperTokenStore } from './innkeeperTokenStore';
+import { UserManager } from 'oidc-client-ts';
 
 export const useInnkeeperOidcStore = defineStore('innkeeperOidcStore', () => {
+  // other stores
+  const { config } = storeToRefs(useConfigStore());
+  const { token } = storeToRefs(useInnkeeperTokenStore());
+
   // private (move to other file maybe?)
   let _userManager: UserManager;
   const _settings: any = {
-    authority: 'https://dev.loginproxy.gov.bc.ca/auth/realms/digitaltrust-nrm',
-    client_id: 'vue3-frontend-local',
-    redirect_uri: 'http://localhost:5173/innkeeper',
+    authority: config.value.frontend.oidc.authority,
+    client_id: config.value.frontend.oidc.client,
+    redirect_uri: `${window.location.origin}/innkeeper`,
     response_type: 'code',
-    automaticSilentRenew: true,
-    post_logout_redirect_uri: 'localhost',
+    automaticSilentRenew: false, // don't need to renew for our needs at this point
+    post_logout_redirect_uri: `${window.location.origin}/innkeeper`,
     loadUserInfo: true,
   };
   _userManager = new UserManager(_settings);
-
-  async function login() {
-    return _userManager.signinRedirect();
-  }
 
   _userManager
     .signinRedirectCallback()
@@ -34,44 +30,52 @@ export const useInnkeeperOidcStore = defineStore('innkeeperOidcStore', () => {
     })
     .catch(function (err) {
       console.error(err);
-      console.log(err);
     });
 
-  _userManager.events.addUserLoaded(() => {
-    console.log('USER LOADED');
-    debugger;
-    _userManager.getUser().then((usr) => {
-      console.log('USER LOADED');
-      user.value = usr;
-      const config = {
-        headers: { Authorization: `Bearer ${usr?.access_token}` },
-      };
-      axios.get('/api/innkeeperLogin', config).then((res) => {
-        console.log(res);
-        debugger;
-        alert(JSON.stringify(res.data));
+  _userManager.events.addUserLoaded(async () => {
+    await _userManager
+      .getUser()
+      .then((usr) => {
+        user.value = usr;
+        const config = {
+          headers: { Authorization: `Bearer ${usr?.access_token}` },
+        };
+        axios
+          .get('/api/innkeeperLogin', config)
+          .then((res) => {
+            token.value = res.data.access_token;
+          })
+          .catch((err) => {
+            error.value = err;
+            console.error(error.value);
+          });
+      })
+      .catch((err) => {
+        error.value = err;
+        console.error(error.value);
       });
-    });
+
+    if (error.value != null) {
+      // throw error so callers can handle appropriately
+      throw error.value;
+    }
   });
 
   // state
-  const token: any = ref(null);
   const loading: any = ref(false);
   const error: any = ref(null);
   const user: any = ref(null);
 
   // getters
-  const innkeeperReady = computed(() => {
-    return token.value != null;
-  });
 
   // actions
+  async function login() {
+    return _userManager.signinRedirect();
+  }
 
   return {
-    token,
     loading,
     error,
-    innkeeperReady,
     login,
     user,
   };
