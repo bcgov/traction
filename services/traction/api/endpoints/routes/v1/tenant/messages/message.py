2645b71eb43c65a20ec637d606a56e27846d9728
Jason Sherman <tools@usingtechnolo.gy>
2022-12-08 11:52:37 -0800

    Handle self-message loop bug by not listening for the event. Deprecate the traction basic message api.
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- services/traction/api/endpoints/routes/v1/tenant/messages/message.py
+++ services/traction/api/endpoints/routes/v1/tenant/messages/message.py
@@ -2,17 +2,17 @@ import logging
 from uuid import UUID
 
 from fastapi import APIRouter
+from fastapi.openapi.models import Response
 from starlette import status
 from starlette.requests import Request
 
-from api.endpoints.dependencies.tenant_security import get_from_context
+
 from api.endpoints.models.v1.messages import (
-    MessageGetResponse,
-    UpdateMessageResponse,
     UpdateMessagePayload,
 )
-from api.endpoints.routes.v1.link_utils import build_item_links
-from api.services.v1 import messages_service
+
+from api.endpoints.routes.v1.tenant.deprecate import sunset_response
+
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
@@ -21,73 +21,40 @@ logger = logging.getLogger(__name__)
 @router.get(
     "/{message_id}",
     status_code=status.HTTP_200_OK,
-    response_model=MessageGetResponse,
+    response_model=Response,
+    deprecated=True,
 )
 async def get_message(
     request: Request,
     message_id: UUID,
     acapy: bool | None = False,
     deleted: bool | None = False,
-) -> MessageGetResponse:
-    wallet_id = get_from_context("TENANT_WALLET_ID")
-    tenant_id = get_from_context("TENANT_ID")
-
-    item = await messages_service.get_message(
-        tenant_id,
-        wallet_id,
-        message_id=message_id,
-        acapy=acapy,
-        deleted=deleted,
-    )
-
-    links = build_item_links(str(request.url), item)
-
-    return MessageGetResponse(item=item, links=links)
+) -> Response:
+    return sunset_response
 
 
 @router.put(
     "/{message_id}",
     status_code=status.HTTP_200_OK,
-    response_model=UpdateMessageResponse,
+    response_model=Response,
+    deprecated=True,
 )
 async def update_message(
     request: Request,
     message_id: UUID,
     payload: UpdateMessagePayload,
-) -> UpdateMessageResponse:
-    wallet_id = get_from_context("TENANT_WALLET_ID")
-    tenant_id = get_from_context("TENANT_ID")
-
-    item = await messages_service.update_message(
-        tenant_id,
-        wallet_id,
-        message_id=message_id,
-        payload=payload,
-    )
-
-    links = build_item_links(str(request.url), item)
-
-    return UpdateMessageResponse(item=item, link=links)
+) -> Response:
+    return sunset_response
 
 
 @router.delete(
     "/{message_id}",
     status_code=status.HTTP_200_OK,
-    response_model=MessageGetResponse,
+    response_model=Response,
+    deprecated=True,
 )
 async def delete_message(
     request: Request,
     message_id: UUID,
-) -> MessageGetResponse:
-    wallet_id = get_from_context("TENANT_WALLET_ID")
-    tenant_id = get_from_context("TENANT_ID")
-
-    item = await messages_service.delete_message(
-        tenant_id,
-        wallet_id,
-        message_id=message_id,
-    )
-
-    links = build_item_links(str(request.url), item)
-
-    return MessageGetResponse(item=item, link=links)
+) -> Response:
+    return sunset_response
--- services/traction/bdd-tests/features/steps/messages.py
+++ services/traction/bdd-tests/features/steps/messages.py
@@ -1,7 +1,14 @@
-import json
+import pprint
+
 from behave import *
-from starlette import status
 from v1_api import *
+
+
+def assert_deprecated(response):
+    assert response.status_code == status.HTTP_410_GONE, response.__dict__
+    assert response.headers.get("sunset"), response.headers
+    assert response.headers.get("deprecated"), response.headers
+    pprint.pp(response.__dict__)
 
 
 @step('"{tenant}" sends "{contact_alias}" a message with content "{content}"')
@@ -10,16 +17,7 @@ def step_impl(context, tenant: str, contact_alias: str, content: str):
     contact = context.config.userdata[tenant]["connections"][contact_alias]
 
     response = send_message(context, tenant, contact["contact_id"], content)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert resp_json["item"]
-    assert resp_json["item"]["content"] == content
-    assert resp_json["item"]["contact"]["alias"] == contact_alias
-    assert resp_json["item"]["role"] == "Sender"
-    context.config.userdata[tenant].setdefault("messages", {})
-    context.config.userdata[tenant]["messages"].setdefault(
-        contact_alias, resp_json["item"]
-    )
+    assert_deprecated(response)
 
 
 @then(
@@ -32,13 +30,7 @@ def step_impl(
 
     params = {"role": role, "contact_id": contact["contact_id"], "tags": tags}
     response = list_messages(context, tenant, params)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert len(resp_json["items"]) == 1, resp_json
-    assert resp_json["items"][0]["contact"]["alias"] == contact_alias
-    _tags = [x.strip() for x in tags.split(",")]
-    for t in _tags:
-        assert t in resp_json["items"][0]["tags"]
+    assert_deprecated(response)
 
 
 @then('"{tenant}" can find {count:d} message(s) as "{role}" with "{contact_alias}"')
@@ -46,21 +38,14 @@ def step_impl(context, tenant: str, count: int, role: str, contact_alias: str):
     contact = context.config.userdata[tenant]["connections"][contact_alias]
     params = {"role": role, "contact_id": contact["contact_id"]}
     response = list_messages(context, tenant, params)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert len(resp_json["items"]) == count, resp_json
-    if count == 1:
-        assert resp_json["items"][0]["contact"]["alias"] == contact_alias
+    assert_deprecated(response)
 
 
 @then('"{tenant}" can get message with "{contact_alias}" by message_id')
 def step_impl(context, tenant: str, contact_alias: str):
     message = context.config.userdata[tenant]["messages"][contact_alias]
     response = get_message(context, tenant, message["message_id"])
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert resp_json["item"]
-    assert resp_json["item"]["contact"]["alias"] == contact_alias
+    assert_deprecated(response)
 
 
 @then('"{tenant}" can update message with "{contact_alias}"')
@@ -77,29 +62,14 @@ def step_impl(context, tenant: str, contact_alias: str):
         payload[attribute] = value
 
     response = update_message(context, tenant, message["message_id"], payload)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    item = resp_json["item"]
-    assert item["message_id"] == message["message_id"]
-    for row in context.table:
-        attribute = row["attribute"]
-        value = row["value"]
-        if attribute == "tags":
-            value = row["value"].split(",")
-
-        assert item[attribute] == value
+    assert_deprecated(response)
 
 
 @then('"{tenant}" can delete message with "{contact_alias}"')
 def step_impl(context, tenant: str, contact_alias: str):
     message = context.config.userdata[tenant]["messages"][contact_alias]
     response = delete_message(context, tenant, message["message_id"])
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    item = resp_json["item"]
-    assert item["message_id"] == message["message_id"]
-    assert item["deleted"]
-    assert item["status"] == "Deleted"
+    assert_deprecated(response)
 
 
 @then('"{tenant}" cannot find message with "{contact_alias}" by role')
@@ -108,9 +78,7 @@ def step_impl(context, tenant: str, contact_alias: str):
     message = context.config.userdata[tenant]["messages"][contact_alias]
     params = {"contact_id": contact["contact_id"], "role": message["role"]}
     response = list_messages(context, tenant, params)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert len(resp_json["items"]) == 0, resp_json
+    assert_deprecated(response)
 
 
 @then('"{tenant}" can find message with "{contact_alias}" by role with deleted flag')
@@ -123,20 +91,14 @@ def step_impl(context, tenant: str, contact_alias: str):
         "deleted": True,
     }
     response = list_messages(context, tenant, params)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert len(resp_json["items"]) == 1, resp_json
-    assert resp_json["items"][0]["contact"]["alias"] == contact_alias
-    assert resp_json["items"][0]["role"] == message["role"]
-    assert resp_json["items"][0]["deleted"]
-    assert resp_json["items"][0]["status"] == "Deleted"
+    assert_deprecated(response)
 
 
 @then('"{tenant}" cannot get message with "{contact_alias}"')
 def step_impl(context, tenant: str, contact_alias: str):
     message = context.config.userdata[tenant]["messages"][contact_alias]
     response = get_message(context, tenant, message["message_id"])
-    assert response.status_code == status.HTTP_404_NOT_FOUND, response.__dict__
+    assert_deprecated(response)
 
 
 @then('"{tenant}" can get message with "{contact_alias}" with deleted flag')
@@ -144,11 +106,7 @@ def step_impl(context, tenant: str, contact_alias: str):
     message = context.config.userdata[tenant]["messages"][contact_alias]
     params = {"deleted": True}
     response = get_message(context, tenant, message["message_id"], params)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert resp_json["item"]["contact"]["alias"] == contact_alias
-    assert resp_json["item"]["deleted"]
-    assert resp_json["item"]["status"] == "Deleted"
+    assert_deprecated(response)
 
 
 @step('"{tenant}" sets configuration "{configuration_name}" to "{flag:bool}"')
@@ -163,19 +121,11 @@ def step_impl(context, tenant: str, configuration_name: str, flag: bool):
 def step_impl(context, tenant: str, role: str):
     params = {"role": role}
     response = list_messages(context, tenant, params)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert resp_json["items"], resp_json
-    for item in resp_json["items"]:
-        assert item["content"] is None, item["content"]
+    assert_deprecated(response)
 
 
 @step('"{tenant}" messages as "{role}" will have content')
 def step_impl(context, tenant: str, role: str):
     params = {"role": role}
     response = list_messages(context, tenant, params)
-    assert response.status_code == status.HTTP_200_OK, response.__dict__
-    resp_json = json.loads(response.content)
-    assert resp_json["items"], resp_json
-    for item in resp_json["items"]:
-        assert item["content"] is not None, item["content"]
+    assert_deprecated(response)
