310e145fce59f14b68296db68ac32e92816fb34f
Jamie Popkin <popkinj@littleearth.ca>
2022-12-08 12:19:28 -0800

    KISS
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

--- services/tenant-ui/frontend/src/views/innkeeper/InnkeeperTenants.vue
+++ services/tenant-ui/frontend/src/views/innkeeper/InnkeeperTenants.vue
@@ -4,4 +4,7 @@
 
 <script setup lang="ts">
 import Tenants from '@/components/innkeeper/tenants/Tenants.vue';
+
+// Override the page title. KISS.
+document.title = 'Traction Innkeeper Console';
 </script>
