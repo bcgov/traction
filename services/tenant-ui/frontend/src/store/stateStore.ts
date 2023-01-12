67b0b62fc2ca593d37e0fd5fa7490832b180104c
Jamie Popkin <popkinj@littleearth.ca>
2023-01-12 10:24:20 -0800

    Hope this state stuff works
    Signed-off-by: Jamie Popkin <popkinj@littleearth.ca>

---
+++ services/tenant-ui/frontend/src/store/stateStore.ts
@@ -0,0 +1,17 @@
+/**
+ * Place for storing global state that is not persisted in the backend.
+ */
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+
+export const useGlobalStateStore = defineStore('globalState', () => {
+  // state
+  const sidebarOpen = ref(null);
+
+  // actions
+  function setSidebarOpen(sidebarState: any) {
+    sidebarOpen.value = sidebarState;
+  }
+
+  return { sidebarOpen, setSidebarOpen };
+});
