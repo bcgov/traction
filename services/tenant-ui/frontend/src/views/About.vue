ce676e8af36bf3d0970a59467a17242196a2d551
Lucas ONeil <lucasoneil@gmail.com>
2023-01-16 13:36:02 -0800

    WIP
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/views/About.vue
+++ services/tenant-ui/frontend/src/views/About.vue
@@ -68,6 +68,15 @@
         </div>
         <div class="w-6 md:w-2 flex justify-content-end">&nbsp;</div>
       </li>
+      <li
+        class="flex align-items-center py-3 px-2 border-top-1 surface-border flex-wrap"
+      >
+        <div class="text-500 w-6 md:w-2 font-medium">ACA-py Version</div>
+        <div class="text-900 w-full md:w-8 md:flex-order-0 flex-order-1">
+          TBD
+        </div>
+        <div class="w-6 md:w-2 flex justify-content-end">&nbsp;</div>
+      </li>
     </ul>
   </div>
 </template>
