b092d17055a9e5863e2c782c0fbfa52e2b1ae94d
Lucas ONeil <lucasoneil@gmail.com>
2023-05-14 20:54:55 -0700

    Add TBD acapy "extra settings" UI
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/components/profile/SettingsForm.vue
+++ services/tenant-ui/frontend/src/components/profile/SettingsForm.vue
@@ -20,7 +20,7 @@
 
       <!-- Webhooks -->
 
-      <Divider class="websockets-top" align="left" type="solid">
+      <Divider class="mb-2" align="left">
         <b>{{ $t('profile.webhooks') }}</b>
       </Divider>
 
@@ -71,12 +71,236 @@
         </TransitionGroup>
       </div>
 
-      <Divider class="websockets-bottom" />
+      <Divider class="mt-2" />
 
       <!-- Image URL -->
       <div class="field">
         <label for="imageUrl">{{ $t('profile.imageUrl') }}</label>
         <InputText id="imageUrl" v-model="v$.imageUrl.$model" class="w-full" />
+      </div>
+
+      <Divider class="mb-2" align="left">
+        <b>{{ $t('tenant.settings.extraSettings') }}</b>
+      </Divider>
+
+      <div class="field">
+        <label for="ACAPY_ENDORSER_ROLE">
+          {{ $t('tenant.settings.acapyEndorserRole') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpEndorserRole')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <Dropdown
+          v-model="v$.ACAPY_ENDORSER_ROLE.$model"
+          :options="endorserRole"
+          class="w-full"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_INVITE_PUBLIC">
+          {{ $t('tenant.settings.acapyInvitePublic') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpInvitePublic')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_INVITE_PUBLIC"
+          v-model="v$.ACAPY_INVITE_PUBLIC.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_LOG_LEVEL">
+          {{ $t('tenant.settings.acapyLogLevel') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpLogLevel')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <Dropdown
+          v-model="v$.ACAPY_LOG_LEVEL.$model"
+          :options="logLevels"
+          class="w-full"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_MONITOR_PING">
+          {{ $t('tenant.settings.acapyMonitorPing') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpMonitorPing')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_MONITOR_PING"
+          v-model="v$.ACAPY_MONITOR_PING.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_NOTIFY_REVOCATION">
+          {{ $t('tenant.settings.acapyNotifyRevocation') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpNotifyRevocation')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_NOTIFY_REVOCATION"
+          v-model="v$.ACAPY_NOTIFY_REVOCATION.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_PUBLIC_INVITES">
+          {{ $t('tenant.settings.acapyPublicInvites') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpPublicInvites')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_PUBLIC_INVITES"
+          v-model="v$.ACAPY_PUBLIC_INVITES.$model"
+        />
+      </div>
+
+      <p>
+        <strong>{{ $t('tenant.settings.auto') }}</strong>
+      </p>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_ACCEPT_INVITES">
+          {{ $t('tenant.settings.acapyAutoAcceptInvites') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpAutoAcceptInvites')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_AUTO_ACCEPT_INVITES"
+          v-model="v$.ACAPY_AUTO_ACCEPT_INVITES.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_ACCEPT_REQUESTS">
+          {{ $t('tenant.settings.acapyAutoAcceptRequests') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpAutoAcceptRequests')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_AUTO_ACCEPT_REQUESTS"
+          v-model="v$.ACAPY_AUTO_ACCEPT_REQUESTS.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_PING_CONNECTION">
+          {{ $t('tenant.settings.acapyAutoPingConnection') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpAutoPingConnection')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_AUTO_PING_CONNECTION"
+          v-model="v$.ACAPY_AUTO_PING_CONNECTION.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_REQUEST_ENDORSEMENT">
+          {{ $t('tenant.settings.acapyAutoRequestEndorsement') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpAutoRequestEndorsement')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id=" ACAPY_AUTO_REQUEST_ENDORSEMENT "
+          v-model="v$.ACAPY_AUTO_REQUEST_ENDORSEMENT.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_RESPOND_CREDENTIAL_OFFER">
+          {{ $t('tenant.settings.acapyAutoRespondCredentialOffer') }}
+          <i
+            v-tooltip="
+              $t('tenant.settings.acapyHelpAutoRespondCredentialOffer')
+            "
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_AUTO_RESPOND_CREDENTIAL_OFFER"
+          v-model="v$.ACAPY_AUTO_RESPOND_CREDENTIAL_OFFER.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_RESPOND_CREDENTIAL_REQUEST">
+          {{ $t('tenant.settings.acapyAutoRespondCredentialRequest') }}
+          <i
+            v-tooltip="
+              $t('tenant.settings.acapyHelpAutoRespondCredentialRequest')
+            "
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_AUTO_RESPOND_CREDENTIAL_REQUEST"
+          v-model="v$.ACAPY_AUTO_RESPOND_CREDENTIAL_REQUEST.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_RESPOND_MESSAGES">
+          {{ $t('tenant.settings.acapyAutoRespondMessages') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpAutoRespondMessages')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_AUTO_RESPOND_MESSAGES"
+          v-model="v$.ACAPY_AUTO_RESPOND_MESSAGES.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_WRITE_TRANSACTIONS">
+          {{ $t('tenant.settings.acapyAutoWriteTransactions') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpAutoWriteTransactions')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id=" ACAPY_AUTO_WRITE_TRANSACTIONS "
+          v-model="v$.ACAPY_AUTO_WRITE_TRANSACTIONS.$model"
+        />
+      </div>
+
+      <div class="field">
+        <label for="ACAPY_AUTO_VERIFY_PRESENTATION">
+          {{ $t('tenant.settings.acapyAutoVerifyPresentation') }}
+          <i
+            v-tooltip="$t('tenant.settings.acapyHelpAutoVerifyPresentation')"
+            class="pi pi-question-circle"
+          />
+        </label>
+        <InputSwitch
+          id="ACAPY_AUTO_VERIFY_PRESENTATION"
+          v-model="v$.ACAPY_AUTO_VERIFY_PRESENTATION.$model"
+        />
       </div>
 
       <div>
@@ -106,6 +330,8 @@ import Accordion from 'primevue/accordion';
 import AccordionTab from 'primevue/accordiontab';
 import Button from 'primevue/button';
 import Divider from 'primevue/divider';
+import Dropdown from 'primevue/dropdown';
+import InputSwitch from 'primevue/inputswitch';
 import InputText from 'primevue/inputtext';
 import Password from 'primevue/password';
 import ProgressSpinner from 'primevue/progressspinner';
@@ -173,11 +399,31 @@ onMounted(async () => {
   loadTenantSettings();
 });
 
+// Dropdown values
+const endorserRole = ref(['author', 'endorser', 'none']);
+const logLevels = ref(['debug', 'info', 'warning', 'error', 'critical']);
+
 // Form Fields and Validation
 const formFields = reactive({
   webhooks: [{ webhookUrl: '', webhookKey: '' }],
   walletLabel: '',
   imageUrl: '',
+  ACAPY_AUTO_ACCEPT_INVITES: false,
+  ACAPY_AUTO_ACCEPT_REQUESTS: false,
+  ACAPY_AUTO_PING_CONNECTION: false,
+  ACAPY_AUTO_REQUEST_ENDORSEMENT: false,
+  ACAPY_AUTO_RESPOND_CREDENTIAL_OFFER: false,
+  ACAPY_AUTO_RESPOND_CREDENTIAL_REQUEST: false,
+  ACAPY_AUTO_RESPOND_MESSAGES: false,
+  ACAPY_AUTO_VERIFY_PRESENTATION: false,
+  ACAPY_AUTO_WRITE_TRANSACTIONS: false,
+  ACAPY_CREATE_REVOCATION_TRANSACTIONS: false,
+  ACAPY_ENDORSER_ROLE: 'author',
+  ACAPY_INVITE_PUBLIC: false,
+  ACAPY_LOG_LEVEL: 'info',
+  ACAPY_MONITOR_PING: false,
+  ACAPY_NOTIFY_REVOCATION: false,
+  ACAPY_PUBLIC_INVITES: false,
 });
 const rules = {
   webhooks: {
@@ -186,6 +432,22 @@ const rules = {
       webhookUrl: { url },
     },
   },
+  ACAPY_AUTO_ACCEPT_INVITES: {},
+  ACAPY_AUTO_ACCEPT_REQUESTS: {},
+  ACAPY_AUTO_PING_CONNECTION: {},
+  ACAPY_AUTO_REQUEST_ENDORSEMENT: {},
+  ACAPY_AUTO_RESPOND_CREDENTIAL_OFFER: {},
+  ACAPY_AUTO_RESPOND_CREDENTIAL_REQUEST: {},
+  ACAPY_AUTO_RESPOND_MESSAGES: {},
+  ACAPY_AUTO_VERIFY_PRESENTATION: {},
+  ACAPY_AUTO_WRITE_TRANSACTIONS: {},
+  ACAPY_CREATE_REVOCATION_TRANSACTIONS: {},
+  ACAPY_ENDORSER_ROLE: {},
+  ACAPY_INVITE_PUBLIC: {},
+  ACAPY_LOG_LEVEL: {},
+  ACAPY_MONITOR_PING: {},
+  ACAPY_NOTIFY_REVOCATION: {},
+  ACAPY_PUBLIC_INVITES: {},
   walletLabel: { required },
   imageUrl: { url },
 };
@@ -236,6 +498,35 @@ const handleSubmit = async (isFormValid: boolean) => {
         }
       });
     }
+
+    const extraSettings = {
+      ACAPY_AUTO_ACCEPT_INVITES: formFields.ACAPY_AUTO_ACCEPT_INVITES,
+      ACAPY_AUTO_ACCEPT_REQUESTS: formFields.ACAPY_AUTO_ACCEPT_REQUESTS,
+      ACAPY_AUTO_PING_CONNECTION: formFields.ACAPY_AUTO_PING_CONNECTION,
+      ACAPY_AUTO_REQUEST_ENDORSEMENT: formFields.ACAPY_AUTO_REQUEST_ENDORSEMENT,
+      ACAPY_AUTO_RESPOND_CREDENTIAL_OFFER:
+        formFields.ACAPY_AUTO_RESPOND_CREDENTIAL_OFFER,
+      ACAPY_AUTO_RESPOND_CREDENTIAL_REQUEST:
+        formFields.ACAPY_AUTO_RESPOND_CREDENTIAL_REQUEST,
+      ACAPY_AUTO_RESPOND_MESSAGES: formFields.ACAPY_AUTO_RESPOND_MESSAGES,
+      ACAPY_AUTO_VERIFY_PRESENTATION: formFields.ACAPY_AUTO_VERIFY_PRESENTATION,
+      ACAPY_AUTO_WRITE_TRANSACTIONS: formFields.ACAPY_AUTO_WRITE_TRANSACTIONS,
+      ACAPY_CREATE_REVOCATION_TRANSACTIONS:
+        formFields.ACAPY_CREATE_REVOCATION_TRANSACTIONS,
+      ACAPY_ENDORSER_ROLE: formFields.ACAPY_ENDORSER_ROLE,
+      ACAPY_INVITE_PUBLIC: formFields.ACAPY_INVITE_PUBLIC,
+      ACAPY_LOG_LEVEL: formFields.ACAPY_LOG_LEVEL,
+      ACAPY_MONITOR_PING: formFields.ACAPY_MONITOR_PING,
+      ACAPY_NOTIFY_REVOCATION: formFields.ACAPY_NOTIFY_REVOCATION,
+      ACAPY_PUBLIC_INVITE: formFields.ACAPY_PUBLIC_INVITES,
+    };
+
+    alert(`
+    "Extra settings", will be added to payload in save call when implemented in acapy.
+    (this to be commented out before merge)
+    ${JSON.stringify(extraSettings, undefined, 2)}
+    `);
+
     const payload = {
       image_url: formFields.imageUrl,
       label: formFields.walletLabel,
@@ -284,13 +575,6 @@ hr {
     visibility: visible;
   }
 }
-.websockets-bottom {
-  margin-top: 0px !important;
-}
-.websockets-top {
-  margin-bottom: 5px !important;
-}
-
 .wh-enter-active,
 .wh-leave-active {
   transition: opacity 0.5s ease;
