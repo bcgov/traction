45c6303c37ff1d2bb651fb121028616e6c83c55c
Akiff Manji <akiff.manji@quartech.com>
2023-05-23 21:28:10 +0000

    feat: add vitest and coverage package
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

---
+++ services/tenant-ui/frontend/hello.test.js
@@ -0,0 +1,25 @@
+import { test, expect } from 'vitest';
+
+function sum(...nums) {
+  return nums.reduce((a, b) => a + b, 0);
+}
+
+test('1 + 1', () => {
+  expect(sum(1, 1)).toEqual(2);
+});
+
+test('1 + 2 + 3', () => {
+  expect(sum(1, 2, 3)).toEqual(6);
+});
+
+test('10 numbers', () => {
+  expect(sum(...Array(10).keys())).toEqual(45);
+});
+
+test('1 number', () => {
+  expect(sum(1)).toEqual(1);
+});
+
+test('0 numbers', () => {
+  expect(sum()).toEqual(0);
+});
