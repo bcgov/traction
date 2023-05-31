fdea5698fa8fb271bef29c29a7deeb8b00987067
Akiff Manji <akiff.manji@quartech.com>
2023-05-31 23:20:23 +0000

    fix: add Vite plugin to load i18n messages
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>

--- services/tenant-ui/frontend/src/plugins/i18n/i18n.ts
+++ services/tenant-ui/frontend/src/plugins/i18n/i18n.ts
@@ -1,30 +1,8 @@
 import { createI18n } from 'vue-i18n';
+import messages from '@intlify/unplugin-vue-i18n/messages';
 
-// The Locale message resources
-function loadLocaleMessages() {
-  // Load messages from any json files in the locales folder
-  const modules = import.meta.glob('./locales/*.json');
-  const messages: any = {};
-  for (const path in modules) {
-    // For each file found, build the message object
-    // with the file NAME as the top level key
-    if (modules[path]) {
-      modules[path]().then((mod: any) => {
-        const matches = path.match(/[ \w-]+?(?=\.)/i);
-        if (matches && matches.length) {
-          messages[matches[0]] = mod;
-        }
-      });
-    }
-  }
-  return messages;
-}
-
-// locale and fallback could be made configurable values later
 export default createI18n({
-  legacy: false,
-  globalInjection: true,
   locale: 'en',
   fallbackLocale: 'en',
-  messages: loadLocaleMessages(),
+  messages,
 });
