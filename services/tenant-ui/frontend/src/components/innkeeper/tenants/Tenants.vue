30faaa3edc28fae517aab74222d267610ad71481
Lucas ONeil <lucasoneil@gmail.com>
2023-06-08 16:40:11 -0700

    Fix tenant table permission flags
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/components/innkeeper/tenants/Tenants.vue
+++ services/tenant-ui/frontend/src/components/innkeeper/tenants/Tenants.vue
@@ -124,6 +124,8 @@ const formattedTenants = computed(() =>
   tenants.value.map((ten: any) => ({
     tenant_id: ten.tenant_id,
     tenant_name: ten.tenant_name,
+    connected_to_endorsers: ten.connected_to_endorsers,
+    created_public_did: ten.created_public_did,
     created: formatDateLong(ten.created_at),
     created_at: ten.created_at,
   }))
