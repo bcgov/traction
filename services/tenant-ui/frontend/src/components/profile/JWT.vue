0059ecdada2f7a6ebb4c0db3e4a952299f7d6f8d
Gavin <gavinfreeborn@gmail.com>
2023-06-14 10:14:45 -0700

    Removed inline styling and store jwt titles as i18n
    Signed-off-by: Gavin <gavinfreeborn@gmail.com>

--- services/tenant-ui/frontend/src/components/profile/JWT.vue
+++ services/tenant-ui/frontend/src/components/profile/JWT.vue
@@ -1,32 +1,32 @@
 <template>
   <h2>{{ $t('profile.token') }}</h2>
   <Accordion :multiple="true">
-    <AccordionTab header="Encoded JWT">
-      <div style="display: flex">
-        <div style="overflow: auto; width: 100%">
+    <AccordionTab :header="$t('common.encodedJwt')">
+      <div class="flex">
+        <div class="overflow-auto w-full">
           <!-- Just used p-inputtext to keep styling consistent -->
-          <div class="p-inputtext" style="overflow: auto">
+          <div class="p-inputtext overflow-auto">
             {{ token }}
           </div>
         </div>
         <Button
           icon="pi pi-copy"
-	        outlined
+          outlined
           text
           @click="copyEncodedToClipboard"
         />
       </div>
     </AccordionTab>
-    <AccordionTab header="Decoded JWT">
-      <div style="display: flex">
-        <div style="overflow: auto; width: 100%">
-          <div class="p-inputtext" style="overflow: auto; display: block">
+    <AccordionTab :header="$t('common.decodedJwt')">
+      <div class="flex">
+        <div class="overflow-auto w-full">
+          <div class="p-inputtext overflow-auto">
             {{ decodedToken }}
           </div>
         </div>
         <Button
           icon="pi pi-copy"
-	        outlined
+          outlined
           text
           @click="copyDecodedToClipboard"
         />
@@ -51,10 +51,10 @@ const { token } = storeToRefs(useTokenStore());
 const decodedToken: Ref<unknown> = computed(() => jwtDecode(token.value ?? ''));
 
 const copyToClipboard = (text: string) => {
-	navigator.clipboard.writeText(text);
-	toast.info('Copied JWT to clipboard!');
+  navigator.clipboard.writeText(text);
+  toast.info('Copied JWT to clipboard!');
 };
 const copyEncodedToClipboard = () => copyToClipboard('Bearer ' + token.value);
 const copyDecodedToClipboard = () =>
-	copyToClipboard(JSON.stringify(decodedToken.value));
+  copyToClipboard(JSON.stringify(decodedToken.value));
 </script>
