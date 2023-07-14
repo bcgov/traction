00bb255de4e301a07c9f83a323e821a6896f387f
jamshale <jamiehalebc@gmail.com>
2023-07-14 14:31:46 -0700

    Add resuable LoadingLable component
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

---
+++ services/tenant-ui/frontend/src/components/common/LoadingLabel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="loading-label">
+    <div v-if="value !== undefined">{{ value }}</div>
+    <div v-else>
+      <ProgressSpinner :style="progressStyle" aria-label="Loading" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType, ref } from 'vue';
+import ProgressSpinner from 'primevue/progressspinner';
+
+const props = defineProps({
+  value: {
+    type: String as PropType<string | undefined>,
+  },
+  size: {
+    type: Number as PropType<number>,
+    default: 1,
+  },
+});
+
+const progressStyle = ref({
+  width: `${props.size}rem`,
+  height: `${props.size}rem`,
+});
+</script>
