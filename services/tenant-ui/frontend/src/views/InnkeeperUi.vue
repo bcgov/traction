77db557ef3654ef4b51c54e79d9c289a374b0f8d
Lucas ONeil <lucasoneil@gmail.com>
2023-10-24 19:59:36 -0700

    Same alert layout in innkeeper
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/views/InnkeeperUi.vue
+++ services/tenant-ui/frontend/src/views/InnkeeperUi.vue
@@ -1,9 +1,13 @@
 <template>
-  <InnkeeperLayout v-if="innkeeperReady" />
-  <InnkeeperLogin v-else />
+  <div class="alert-container">
+    <Alert class="z-2" />
+    <InnkeeperLayout v-if="innkeeperReady" />
+    <InnkeeperLogin v-else />
+  </div>
 </template>
 
 <script setup lang="ts">
+import Alert from '@/components/notifications/Alert.vue';
 import InnkeeperLayout from '@/components/layout/innkeeper/InnkeeperLayout.vue';
 import InnkeeperLogin from '@/components/innkeeper/InnkeeperLogin.vue';
 import { storeToRefs } from 'pinia';
@@ -11,3 +15,10 @@ import { useInnkeeperTokenStore } from '@/store';
 
 const { innkeeperReady } = storeToRefs(useInnkeeperTokenStore());
 </script>
+
+<style scoped lang="scss">
+.p-message {
+  margin: 0;
+  border-radius: 0;
+}
+</style>
