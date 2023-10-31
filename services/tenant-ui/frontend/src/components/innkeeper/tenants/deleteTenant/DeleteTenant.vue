5595b066b818fdf9772a5d2aa0160ce5299af589
Gurjeet Matharu <gmatharu12@gmail.com>
2023-10-31 09:33:59 -0700

    DeleteTenant.vue clean up
    Signed-off-by: Gurjeet Matharu <gmatharu12@gmail.com>

--- services/tenant-ui/frontend/src/components/innkeeper/tenants/deleteTenant/DeleteTenant.vue
+++ services/tenant-ui/frontend/src/components/innkeeper/tenants/deleteTenant/DeleteTenant.vue
@@ -23,31 +23,26 @@
 </template>
 
 <script setup lang="ts">
-// Types
-import { TenantRecord } from '@/types/acapyApi/acapyInterface';
-
-// Vue
 import { ref } from 'vue';
-// PrimeVue
+
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
-// Custom Components
+
 import ConfirmTenantDeletion from './ConfirmTenantDeletion.vue';
+
+import { TenantRecord } from '@/types/acapyApi/acapyInterface';
 
 defineEmits(['success']);
 
-// Props
 const props = defineProps<{
   tenant: TenantRecord;
 }>();
 
 const displayModal = ref(false);
 const openModal = async () => {
-  // Kick of the loading asyncs (if needed)
   displayModal.value = true;
 };
 const handleClose = async () => {
-  // some logic... maybe we shouldn't close?
   displayModal.value = false;
 };
 </script>
