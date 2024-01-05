227be74307ccb1db99e71026a6841394bd373303
Lucas ONeil <lucasoneil@gmail.com>
2024-01-04 16:31:32 -0800

    Fix mock
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/tenant-ui/frontend/src/helpers/index.ts
+++ services/tenant-ui/frontend/src/helpers/index.ts
@@ -21,7 +21,7 @@ function i18n2DateLocale(i18nLocale: I18nLocale): Locale {
 }
 
 function _dateFnsFormat(value: string, formatter: string) {
-  const locale: any = i18n.global.locale;
+  const locale: any = i18n.global?.locale || '';
   const formatted = '';
   try {
     if (value) {
