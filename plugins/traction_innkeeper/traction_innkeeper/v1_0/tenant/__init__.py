fea0253f5010210d56f6a159abdb5d547ba15eb6
Akiff Manji <akiff.manji@quartech.com>
2024-06-06 14:53:07 -0700

    feat: add tenant id to log output (#1065)
    * feat: add tenant id to log output
    
    ---------
    
    Signed-off-by: Akiff Manji <akiff.manji@quartech.com>
    Signed-off-by: Akiff Manji <amanji@petridish.dev>
    Co-authored-by: jamshale <31809382+jamshale@users.noreply.github.com>
    Co-authored-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- plugins/traction_innkeeper/traction_innkeeper/v1_0/tenant/__init__.py
+++ plugins/traction_innkeeper/traction_innkeeper/v1_0/tenant/__init__.py
@@ -1,14 +1,63 @@
+from contextvars import ContextVar
 import logging
 
 from aries_cloudagent.config.injection_context import InjectionContext
 from aries_cloudagent.core.event_bus import EventBus
 from aries_cloudagent.core.plugin_registry import PluginRegistry
 from aries_cloudagent.core.protocol_registry import ProtocolRegistry
+from aries_cloudagent.config.logging import TimedRotatingFileMultiProcessHandler
+
+from pythonjsonlogger import jsonlogger
 
 from .holder_revocation_service import subscribe, HolderRevocationService
 
 LOGGER = logging.getLogger(__name__)
 
+LOG_FORMAT_PATTERN = (
+    "%(asctime)s  TENANT: %(tenant_id)s %(levelname)s %(pathname)s:%(lineno)d %(message)s"
+)
+
+base_log_record_factory = logging.getLogRecordFactory()
+
+class ContextFilter(logging.Filter):
+    """Custom logging filter to adapt logs with contextual tenant_id."""
+
+    def __init__(self):
+        """Initialize an instance of logging filter."""
+
+        super(ContextFilter, self).__init__()
+
+    def filter(self, record):
+        """Filter log records and add tenant id to them."""
+
+        if not hasattr(record, 'tenant_id') or not record.tenant_id:
+            record.tenant_id = None
+        return True
+
+
+def setup_multitenant_logging():
+    """Setup method for multitenant logging"""
+
+    root_logger = logging.getLogger()
+    for handler in root_logger.handlers:
+        handler.setFormatter(
+            logging.Formatter(LOG_FORMAT_PATTERN)
+        )
+        handler.addFilter(ContextFilter())
+
+
+def log_records_inject(tenant_id: str):
+    """Injects tenant_id into log records"""
+
+    try:
+        def modded_log_record_factory(*args, **kwargs):
+            record = base_log_record_factory(*args, **kwargs)
+            record.tenant_id = tenant_id
+            return record
+
+        logging.setLogRecordFactory(modded_log_record_factory)
+    except Exception as e: # pylint: disable=broad-except
+        LOGGER.error("There was a problem injecting tenant_id into the logs: %s", e)
 
 async def setup(context: InjectionContext):
     LOGGER.info("> plugin setup...")
@@ -29,5 +78,7 @@ async def setup(context: InjectionContext):
     context.injector.bind_instance(HolderRevocationService, srv)
 
     subscribe(bus)
+
+    setup_multitenant_logging()
 
     LOGGER.info("< plugin setup.")
