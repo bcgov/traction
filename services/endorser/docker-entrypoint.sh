0abf810bbeb9d2c9aa8e2f19aa8213fa638c940c
Lucas ONeil <lucasoneil@gmail.com>
2024-08-29 09:45:54 -0700

    Endorser api removal
    Signed-off-by: Lucas ONeil <lucasoneil@gmail.com>

--- services/endorser/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# python3 -m alembic upgrade head
-# if [ $? == 0 ]; then
-    exec uvicorn api.main:app --host 0.0.0.0 --port $ENDORSER_API_PORT --log-level error
-# fi
-# echo 'Alembic db upgrade failed...'
-# exit 1
