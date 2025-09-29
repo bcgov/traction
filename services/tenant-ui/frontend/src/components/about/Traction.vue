26b58c95ccfb62b1c19cf47736348c41019ad812
Emiliano Suñé <emiliano.sune@gmail.com>
2025-09-29 11:28:02 -0700

    Bump app version
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- services/tenant-ui/frontend/src/components/about/Traction.vue
+++ services/tenant-ui/frontend/src/components/about/Traction.vue
@@ -3,10 +3,10 @@
     <div class="col-12 md:col-6">
       <strong>{{ $t('about.traction.info') }}</strong>
       <p class="my-0">
-        {{ $t('about.traction.tractionVersion', { version: '1.2.0' }) }}
+        {{ $t('about.traction.tractionVersion', { version: '1.2.1' }) }}
       </p>
       <p class="mt-0">
-        {{ $t('about.traction.uiVersion', { version: '1.2.0' }) }}
+        {{ $t('about.traction.uiVersion', { version: '1.2.1' }) }}
       </p>
     </div>
 
--- services/tenant-ui/frontend/test/__mocks__/store/tenant.ts
+++ services/tenant-ui/frontend/test/__mocks__/store/tenant.ts
@@ -13,7 +13,7 @@ const store: { [key: string]: any } = {
   serverConfig: {
     value: {
       config: {
-        version: '1.2.0',
+        version: '1.2.1',
       },
     },
   },
